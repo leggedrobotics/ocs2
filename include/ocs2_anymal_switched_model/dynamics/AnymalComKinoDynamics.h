f53b2ece009bb6e4c99486fbfd83f0d98781624a
Farbod Farshidian <farbod.farshidian@gmail.com>
2017-11-18 23:52:01 +0100

    eigen 3 as external lib.

--- include/ocs2_anymal_switched_model/dynamics/AnymalComKinoDynamics.h
+++ include/ocs2_anymal_switched_model/dynamics/AnymalComKinoDynamics.h
@@ -2,7 +2,8 @@
 #define ANYMAL_COMKINODYNAMICS_H_
 
 #include <c_switched_model_interface/dynamics/ComKinoDynamicsBase.h>
-#include <ocs2_anymal_switched_model/AnymalSwitchedModel.h>
+#include <c_switched_model_interface/core/Options.h>
+#include <c_switched_model_interface/core/MotionPhaseDefinition.h>
 #include <ocs2_anymal_switched_model/dynamics/AnymalCom.h>
 #include <ocs2_anymal_switched_model/kinematics/AnymalKinematics.h>
 
@@ -14,7 +15,7 @@ public:
 	typedef switched_model::ComKinoDynamicsBase<12> Base;
 
 	AnymalComKinoDynamics(const std::array<bool,4>& stanceLegs,
-		const double& gravitationalAcceleration=9.81, const switched_model::Options& options = Options(),
+		const double& gravitationalAcceleration=9.81, const switched_model::Options& options = switched_model::Options(),
 		const switched_model::FeetZDirectionPlannerBase::Ptr& feetZDirectionPlanner=NULL,
 		const std::vector<switched_model::EndEffectorConstraintBase::Ptr>& endEffectorStateConstraints = std::vector<switched_model::EndEffectorConstraintBase::Ptr>())
 
--- include/ocs2_anymal_switched_model/dynamics/AnymalComKinoDynamicsDerivative.h
+++ include/ocs2_anymal_switched_model/dynamics/AnymalComKinoDynamicsDerivative.h
@@ -2,7 +2,8 @@
 #define ANYMAL_COMKINODYNAMICSDERIVATIVE_H_
 
 #include <c_switched_model_interface/dynamics_derivative/ComKinoDynamicsDerivativeBase.h>
-#include <ocs2_anymal_switched_model/AnymalSwitchedModel.h>
+#include <c_switched_model_interface/core/Options.h>
+#include <c_switched_model_interface/core/MotionPhaseDefinition.h>
 #include <ocs2_anymal_switched_model/dynamics/AnymalCom.h>
 #include <ocs2_anymal_switched_model/kinematics/AnymalKinematics.h>
 
@@ -14,7 +15,7 @@ public:
 	typedef ComKinoDynamicsDerivativeBase<12> Base;
 
 	AnymalComKinoDynamicsDerivative(const std::array<bool,4>& stanceLegs,
-		const double& gravitationalAcceleration=9.81, const switched_model::Options& options = Options(),
+		const double& gravitationalAcceleration=9.81, const switched_model::Options& options = switched_model::Options(),
 		const switched_model::FeetZDirectionPlannerBase::Ptr& feetZDirectionPlanner=NULL,
 		const std::vector<switched_model::EndEffectorConstraintBase::Ptr>& endEffectorStateConstraints = std::vector<switched_model::EndEffectorConstraintBase::Ptr>())
 

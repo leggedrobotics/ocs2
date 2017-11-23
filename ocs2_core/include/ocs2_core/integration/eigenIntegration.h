9ce7aadb655862d3101cb7707ec1a9ffcd8fdce7
Farbod Farshidian <farbod.farshidian@gmail.com>
2017-11-23 14:15:45 +0100

    Boost odeint bug fixed.

--- ocs2_core/include/ocs2_core/integration/eigenIntegration.h
+++ ocs2_core/include/ocs2_core/integration/eigenIntegration.h
@@ -126,7 +126,8 @@ struct vector_space_norm_inf<Eigen::Matrix<double, S1, S2, O, M1, M2>>
 {
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	double operator()(const Eigen::Matrix<double, S1, S2, O, M1, M2> &m) const
+	typedef double result_type;
+	result_type operator()(const Eigen::Matrix<double, S1, S2, O, M1, M2> &m) const
     {
         return m.template lpNorm<Eigen::Infinity>();
     }

85bcbfc98863a7c6d56958123dd8c7daa8ba1593
Farbod Farshidian <farbod.farshidian@gmail.com>
2018-07-27 17:47:33 +0200

    non-realtime MPC loop is implemented.

--- test/DummyMRT.cpp
+++ test/DummyMRT.cpp
@@ -8,10 +8,9 @@
 #include <string>
 #include <iostream>
 
-#include <boost/filesystem.hpp>
-
 #include "ocs2_anymal_interface/OCS2AnymalInterface.h"
 #include <ocs2_quadruped_interface/test/MRT_ROS_Dummy_Quadruped.h>
+#include <pathfile.h>
 
 using namespace anymal;
 
@@ -25,16 +24,18 @@ int main( int argc, char* argv[] )
 
 	/******************************************************************************************************/
 	if ( argc <= 1) throw std::runtime_error("No task file specified. Aborting.");
-	boost::filesystem::path filePath(__FILE__);
-	std::string taskFile = filePath.parent_path().parent_path().generic_string() + "/config/" + std::string(argv[1]) + "/task.info";
-	std::cout << "Loading task file: " << taskFile << std::endl;
+	std::string taskFile = std::string(PACKAGE_PATH) + "/config/" + std::string(argv[1]) + "/task.info";
+	std::cerr << "Loading task file: " << taskFile << std::endl;
 
 	ocs2_robot_interface_t::Ptr optimizationInterfacePtr( new ocs2_robot_interface_t(taskFile) );
 	ocs2_robot_interface_t::rbd_state_vector_t initRbdState;
 	optimizationInterfacePtr->getLoadedInitialState(initRbdState);
 
 	switched_model::MRT_ROS_Dummy_Quadruped<12> dummySimulator(
-			optimizationInterfacePtr, mrtLoopFrequency, robotName);
+			optimizationInterfacePtr,
+			optimizationInterfacePtr->getMpcSettings().mrtDesiredFrequency_,
+			robotName,
+			optimizationInterfacePtr->getMpcSettings().mpcDesiredFrequency_);
 
 	dummySimulator.launchNodes(argc, argv);
 

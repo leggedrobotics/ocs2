4d52d2f834a1c33f21504cdb4872d9553ce98ac4
Farbod Farshidian <farbod.farshidian@gmail.com>
2018-08-24 10:22:09 +0200

    example 2 is added to slq

---
+++ ocs2_slq/test/exp2_slq_test.cpp
@@ -0,0 +1,139 @@
+/******************************************************************************
+Copyright (c) 2017, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <gtest/gtest.h>
+
+#include <ocs2_slq/SLQ.h>
+#include <ocs2_slq/SLQ_MP.h>
+#include <ocs2_slq/test/EXP2.h>
+
+using namespace ocs2;
+
+enum
+{
+	STATE_DIM = 2,
+	INPUT_DIM = 2
+};
+
+TEST(exp2_slq_test, Exp2_slq_test)
+{
+
+	// system dynamics
+	EXP2_System systemDynamics;
+
+	// system derivatives
+	EXP2_SystemDerivative systemDerivative;
+
+	// system constraints
+	EXP2_constraint systemConstraint;
+
+	// system cost functions
+	EXP2_CostFunction systemCostFunction;
+
+	// system operatingTrajectories
+	Eigen::Matrix<double,STATE_DIM,1> stateOperatingPoint = Eigen::Matrix<double,STATE_DIM,1>::Zero();
+	Eigen::Matrix<double,INPUT_DIM,1> inputOperatingPoint = Eigen::Matrix<double,INPUT_DIM,1>::Zero();
+	EXP2_SystemOperatingTrajectories operatingTrajectories(stateOperatingPoint, inputOperatingPoint);
+
+
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	SLQ_Settings slqSettings;
+	slqSettings.displayInfo_ = true;
+	slqSettings.displayShortSummary_ = true;
+	slqSettings.maxNumIterationsSLQ_ = 2;
+	slqSettings.absTolODE_ = 1e-10;
+	slqSettings.relTolODE_ = 1e-7;
+	slqSettings.maxNumStepsPerSecond_ = 10000;
+	slqSettings.nThreads_ = 3;
+	slqSettings.useMakePSD_ = true;
+	slqSettings.maxNumIterationsSLQ_ = 30;
+	slqSettings.lsStepsizeGreedy_ = true;
+	slqSettings.noStateConstraints_ = true;
+	slqSettings.useNominalTimeForBackwardPass_ = false;
+
+	// switching times
+	std::vector<double> eventTimes {0.2, 1.2};
+	EXP2_LogicRules logicRules(eventTimes);
+
+	double startTime = 0.0;
+	double finalTime = 3.0;
+
+	// partitioning times
+	std::vector<double> partitioningTimes;
+	partitioningTimes.push_back(startTime);
+	partitioningTimes.push_back(1.0);
+	partitioningTimes.push_back(2.0);
+	partitioningTimes.push_back(finalTime);
+
+	Eigen::Vector2d initState(2.0, 3.0);
+
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	// SLQ - single core version
+	SLQ<STATE_DIM, INPUT_DIM, EXP2_LogicRules> slq(
+			&systemDynamics, &systemDerivative,
+			&systemConstraint, &systemCostFunction,
+			&operatingTrajectories, slqSettings, &logicRules);
+
+	// run single core SLQ
+	if (slqSettings.displayInfo_ || slqSettings.displayShortSummary_)
+		std::cerr << "\n>>> single-core SLQ" << std::endl;
+	slq.run(startTime, initState, finalTime, partitioningTimes);
+
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	// get controller
+	SLQ_BASE<STATE_DIM, INPUT_DIM, EXP2_LogicRules>::controller_array_t controllersStock = slq.getController();
+
+	// get performance indices
+	double totalCost, totalCost_mp;
+	double constraint1ISE, constraint1ISE_mp;
+	double constraint2ISE, constraint2ISE_mp;
+	slq.getPerformanceIndeces(totalCost, constraint1ISE, constraint2ISE);
+	slq.getPerformanceIndeces(totalCost_mp, constraint1ISE_mp, constraint2ISE_mp);
+
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+	/******************************************************************************************************/
+}
+
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+

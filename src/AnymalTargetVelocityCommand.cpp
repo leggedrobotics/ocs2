6937c5695adf999f15f9958485aaa5e9ffd3c264
rubengrandia <rubengrandia@gmail.com>
2018-09-06 10:40:16 +0200

    Change angle limits in velocity mode

--- src/AnymalTargetVelocityCommand.cpp
+++ src/AnymalTargetVelocityCommand.cpp
@@ -12,7 +12,7 @@ int main( int argc, char* argv[] )
   using quadrupedKeyboard = switched_model::TargetTrajectories_Keyboard_Quadruped<double>;
   auto command_mode = quadrupedKeyboard::COMMAND_MODE::VELOCITY;
   quadrupedKeyboard::scalar_array_t command_limits = quadrupedKeyboard::scalar_array_t{
-    2.0, 2.0, 0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0};
+    2.0, 2.0, 0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0, 40.0, 40.0, 360.0};
 	switched_model::TargetTrajectories_Keyboard_Quadruped<double> targetPoseCommand("anymal", command_limits, command_mode);
 
 	targetPoseCommand.launchNodes(argc, argv);

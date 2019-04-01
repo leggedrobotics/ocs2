497f1ac77a00c6d8ac483cecb49d0b7d77a6951c
rgrandia <rubengrandia@gmail.com>
2019-04-01 16:48:55 +0200

    extracted joystick interface from feature/feedback

---
+++ ocs2_robotic_examples/ocs2_robotic_tools/include/ocs2_robotic_tools/command/TargetTrajectories_Joystick_Interface.h
@@ -0,0 +1,110 @@
+/******************************************************************************
+Copyright (c) 2017, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#ifndef TARGETTRAJECTORIES_JOYSTICK_INTERFACE_OCS2_H_
+#define TARGETTRAJECTORIES_JOYSTICK_INTERFACE_OCS2_H_
+
+#include <vector>
+#include <csignal>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Eigen/Core>
+
+#include <ocs2_comm_interfaces/ocs2_ros_interfaces/command/TargetTrajectories_ROS_Interface.h>
+
+namespace ocs2 {
+
+/**
+ * This class lets the user to insert robot command form joystick.
+ *
+ * @tparam SCALAR_T: scalar type.
+ */
+template <typename SCALAR_T>
+class TargetTrajectories_Joystick_Interface : public ocs2::TargetTrajectories_ROS_Interface<SCALAR_T>
+{
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+	typedef ocs2::TargetTrajectories_ROS_Interface<SCALAR_T> BASE;
+	typedef typename BASE::scalar_t scalar_t;
+	typedef typename BASE::scalar_array_t scalar_array_t;
+	typedef typename BASE::dynamic_vector_t dynamic_vector_t;
+	typedef typename BASE::dynamic_vector_array_t dynamic_vector_array_t;
+	typedef typename BASE::cost_desired_trajectories_t cost_desired_trajectories_t;
+
+	/**
+	 * Constructor
+	 *
+	 * @param [in] robotName: robot's name.
+	 * @param [in] targetCommandSize: command expected length
+	 * @param [in] targetCommandLimits: The limits of the loaded command from
+	 * command-line (for safety purposes).
+	 */
+	TargetTrajectories_Joystick_Interface(
+			const std::string& robotName = "robot",
+			const size_t targetCommandSize = 0,
+			const scalar_array_t& targetCommandLimits = scalar_array_t());
+
+	/**
+	 * Default destructor
+	 */
+	virtual ~TargetTrajectories_Joystick_Interface() = default;
+
+	/**
+	 * Gets the command vector size.
+	 *
+	 * @return The command vector size.
+	 */
+	size_t& targetCommandSize();
+
+	/**
+	 * Sets cost trajectories from input desired state, and publishes cost desired trajectories
+	 */
+	void publishTargetTrajectoriesFromDesiredState(dynamic_vector_t desiredState);
+
+
+protected:
+
+
+/******
+ * Variables
+ ******/
+	size_t targetCommandSize_;
+	scalar_array_t targetCommandLimits_;
+
+	scalar_array_t targetCommand_;
+
+};
+
+} // namespace ocs2
+
+#include "implementation/TargetTrajectories_Joystick_Interface.h"
+
+#endif /* TARGETTRAJECTORIES_JOYSTCK_INTERFACE_OCS2_H_ */
---
+++ ocs2_robotic_examples/ocs2_robotic_tools/include/ocs2_robotic_tools/command/implementation/TargetTrajectories_Joystick_Interface.h
@@ -0,0 +1,77 @@
+/******************************************************************************
+Copyright (c) 2017, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+namespace ocs2{
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+template <typename SCALAR_T>
+TargetTrajectories_Joystick_Interface<SCALAR_T>::TargetTrajectories_Joystick_Interface(
+		const std::string& robotName /*= "robot"*/,
+		const size_t targetCommandSize /*= 0*/,
+		const scalar_array_t& targetCommandLimits /*= scalar_array_t()*/)
+	: BASE(robotName)
+	, targetCommandSize_(targetCommandSize)
+	, targetCommandLimits_(targetCommandLimits)
+{
+	if (targetCommandLimits.size()!=targetCommandSize)
+		throw std::runtime_error("Target command limits are not set properly");
+}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+template <typename SCALAR_T>
+size_t& TargetTrajectories_Joystick_Interface<SCALAR_T>::targetCommandSize() {
+
+	return targetCommandSize_;
+}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+template <typename SCALAR_T>
+void TargetTrajectories_Joystick_Interface<SCALAR_T>::publishTargetTrajectoriesFromDesiredState(dynamic_vector_t desiredState) {
+
+	// user defined modification of the joystick commands
+	cost_desired_trajectories_t costDesiredTrajectories(1);
+	// time
+	costDesiredTrajectories.desiredTimeTrajectory()[0]= 0.0;
+	// state -- this is setup in adjustTargetTrajectories in the robot_interface
+	costDesiredTrajectories.desiredStateTrajectory()[0] = desiredState;
+	// input
+	costDesiredTrajectories.desiredInputTrajectory()[0] = dynamic_vector_t::Zero(0);
+
+	// publish cost desired trajectories
+	BASE::publishTargetTrajectories(costDesiredTrajectories);
+
+
+}
+} // namespace ocs2

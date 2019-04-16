aea6f9424f7f59f9e79e628d5c205091019b3bb0
Jan Carius <jan.carius@gmx.de>
2019-04-16 17:36:07 +0000

    disable exp0_slq_test due to buildserver issues

--- ocs2_slq/test/exp0_slq_test.cpp
+++ ocs2_slq/test/exp0_slq_test.cpp
@@ -43,7 +43,7 @@ enum {
   INPUT_DIM = 1
 };
 
-TEST(exp0_slq_test, exp0_slq_test_single_core) {
+TEST(exp0_slq_test, DISABLED_exp0_slq_test_single_core) {
   // system dynamics
   EXP0_System systemDynamics;
 

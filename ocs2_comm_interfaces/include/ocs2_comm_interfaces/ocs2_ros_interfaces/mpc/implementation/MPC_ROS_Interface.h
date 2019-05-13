192c3d99184a5f91c21d76bfcb3124815e8fc7a7
Farbod Farshidian <farbod.farshidian@gmail.com>
2019-05-13 14:25:35 +0200

    Ballbot example is fixed.

--- ocs2_comm_interfaces/include/ocs2_comm_interfaces/ocs2_ros_interfaces/mpc/implementation/MPC_ROS_Interface.h
+++ ocs2_comm_interfaces/include/ocs2_comm_interfaces/ocs2_ros_interfaces/mpc/implementation/MPC_ROS_Interface.h
@@ -252,6 +252,7 @@ void MPC_ROS_Interface<STATE_DIM, INPUT_DIM, LOGIC_RULES_T>::publishPolicy(
 		std::vector<std::vector<float>*> policyMsgDataPointers;
 		policyMsgDataPointers.reserve(N);
 
+		scalar_array_t timeTrajectoryTruncated;
 		for (size_t k=0; k<N; k++) { // loop through time in partition i
 			// continue if elapsed time is smaller than computation time delay
 			if (k<N-1 && timeTrajectory[k+1]<t0)  continue;
@@ -266,9 +267,11 @@ void MPC_ROS_Interface<STATE_DIM, INPUT_DIM, LOGIC_RULES_T>::publishPolicy(
 
 			mpcPolicyMsg_.data.emplace_back(ocs2_comm_interfaces::controller_data());
 			policyMsgDataPointers.push_back(&mpcPolicyMsg_.data.back().data);
+			timeTrajectoryTruncated.push_back(timeTrajectory[k]);
 		}  // end of k loop
 
-		ctrlToBeSent->flatten(timeTrajectory, policyMsgDataPointers);
+
+		ctrlToBeSent->flatten(timeTrajectoryTruncated, policyMsgDataPointers);
 	}  // end of i loop
 
 #ifdef PUBLISH_THREAD

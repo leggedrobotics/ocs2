19ca551edbf5107b8a11f47dee1b720a89eee832
Jan Carius <jan.carius@gmx.de>
2019-06-19 18:50:23 +0200

    create macro for binding generic robot interface

--- ocs2_robotic_examples/ocs2_double_integrator_noros_example/src/pyBindModule3.cpp
+++ ocs2_robotic_examples/ocs2_double_integrator_noros_example/src/pyBindModule3.cpp
@@ -28,32 +28,37 @@ using namespace pybind11::literals;
       .def("__iter__", [](VTYPE& v) { return pybind11::make_iterator(v.begin(), v.end()); },\
            pybind11::keep_alive<0, 1>()); /* Keep vector alive while iterator is used */
 
-PYBIND11_MAKE_OPAQUE(DoubleIntegratorPyBindings::scalar_array_t);
-PYBIND11_MAKE_OPAQUE(DoubleIntegratorPyBindings::state_vector_array_t);
-PYBIND11_MAKE_OPAQUE(DoubleIntegratorPyBindings::input_vector_array_t);
-PYBIND11_MAKE_OPAQUE(DoubleIntegratorPyBindings::state_matrix_array_t);
+// convenience macro to bind robot interface with all required vectors. LIB_NAME must match target name in CMakeLists
+#define CREATE_ROBOT_PYTHON_BINDINGS(PY_INTERFACE, LIB_NAME)\
+  /* make vector types opaque so they are not converted to python lists */\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::scalar_array_t);\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::state_vector_array_t);\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::input_vector_array_t);\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::state_matrix_array_t);\
+  /* create a python module */\
+  PYBIND11_MODULE(LIB_NAME, m) {\
+    /* bind vector types so they can be used natively in python */\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::scalar_array_t, "scalar_array")\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::state_vector_array_t, "state_vector_array")\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::input_vector_array_t, "input_vector_array_t")\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::state_matrix_array_t, "state_matrix_array_t")\
+    /* bind the actual mpc interface */\
+    pybind11::class_<PY_INTERFACE>(m, "mpc_interface")\
+        .def(pybind11::init<const std::string&, bool>())\
+        .def_property_readonly_static("STATE_DIM", [](pybind11::object) {return static_cast<int>(PY_INTERFACE::dim_t::DIMS::STATE_DIM_);})\
+        .def_property_readonly_static("INPUT_DIM", [](pybind11::object) {return static_cast<int>(PY_INTERFACE::dim_t::DIMS::INPUT_DIM_);})\
+        .def("setObservation", &PY_INTERFACE::setObservation, "t"_a, "x"_a.noconvert())\
+        .def("advanceMpc", &PY_INTERFACE::advanceMpc)\
+        .def("getMpcSolution", &PY_INTERFACE::getMpcSolution, "t"_a.noconvert(), "x"_a.noconvert(), "u"_a.noconvert(), "sigmaX"_a.noconvert())\
+        .def("computeFlowMap", &PY_INTERFACE::computeFlowMap, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("setFlowMapDerivativeStateAndControl", &PY_INTERFACE::setFlowMapDerivativeStateAndControl, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("computeFlowMapDerivativeState", &PY_INTERFACE::computeFlowMapDerivativeState)\
+        .def("computeFlowMapDerivativeInput", &PY_INTERFACE::computeFlowMapDerivativeInput)\
+        .def("getRunningCost", &PY_INTERFACE::getRunningCost, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("getRunningCostDerivativeState", &PY_INTERFACE::getRunningCostDerivativeState, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("getRunningCostDerivativeInput", &PY_INTERFACE::getRunningCostDerivativeInput, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("getValueFunctionStateDerivative", &PY_INTERFACE::getValueFunctionStateDerivative, "t"_a, "x"_a.noconvert());\
+    }
 
-PYBIND11_MODULE(DoubleIntegratorPyBindings3, m) {
-  // bind the actual interface
-  pybind11::class_<DoubleIntegratorPyBindings>(m, "mpc_interface")
-      .def(pybind11::init<const std::string&, bool>())
-      .def_property_readonly_static("STATE_DIM", [](pybind11::object) {return static_cast<int>(double_integrator_dims::STATE_DIM_);})
-      .def_property_readonly_static("INPUT_DIM", [](pybind11::object) {return static_cast<int>(double_integrator_dims::INPUT_DIM_);})
-      .def("setObservation", &DoubleIntegratorPyBindings::setObservation, "t"_a, "x"_a.noconvert())
-      .def("advanceMpc", &DoubleIntegratorPyBindings::advanceMpc)
-      .def("getMpcSolution", &DoubleIntegratorPyBindings::getMpcSolution, "t"_a.noconvert(), "x"_a.noconvert(), "u"_a.noconvert(), "sigmaX"_a.noconvert())
-      .def("computeFlowMap", &DoubleIntegratorPyBindings::computeFlowMap, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())
-      .def("setFlowMapDerivativeStateAndControl", &DoubleIntegratorPyBindings::setFlowMapDerivativeStateAndControl, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())
-      .def("computeFlowMapDerivativeState", &DoubleIntegratorPyBindings::computeFlowMapDerivativeState)
-      .def("computeFlowMapDerivativeInput", &DoubleIntegratorPyBindings::computeFlowMapDerivativeInput)
-      .def("getRunningCost", &DoubleIntegratorPyBindings::getRunningCost, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())
-      .def("getRunningCostDerivativeState", &DoubleIntegratorPyBindings::getRunningCostDerivativeState, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())
-      .def("getRunningCostDerivativeInput", &DoubleIntegratorPyBindings::getRunningCostDerivativeInput, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())
-      .def("getValueFunctionStateDerivative", &DoubleIntegratorPyBindings::getValueFunctionStateDerivative, "t"_a, "x"_a.noconvert());
 
-  // bind vector types so they can be used natively in python
-  VECTOR_TYPE_BINDING(DoubleIntegratorPyBindings::scalar_array_t, "scalar_array")
-  VECTOR_TYPE_BINDING(DoubleIntegratorPyBindings::state_vector_array_t, "state_vector_array")
-  VECTOR_TYPE_BINDING(DoubleIntegratorPyBindings::input_vector_array_t, "input_vector_array_t")
-  VECTOR_TYPE_BINDING(DoubleIntegratorPyBindings::state_matrix_array_t, "state_matrix_array_t")
-}
+CREATE_ROBOT_PYTHON_BINDINGS(DoubleIntegratorPyBindings, DoubleIntegratorPyBindings3)

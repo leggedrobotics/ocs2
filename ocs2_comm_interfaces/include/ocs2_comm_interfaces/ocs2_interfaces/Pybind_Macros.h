804362217a7b1b0094c465016cbdc998c3147e3c
Jan Carius <jan.carius@gmx.de>
2019-06-20 14:59:21 +0200

    move pybind macros to separate header file

---
+++ ocs2_comm_interfaces/include/ocs2_comm_interfaces/ocs2_interfaces/Pybind_Macros.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+using namespace pybind11::literals;
+
+// convenience macro to bind all kinds of std::vector-like types
+#define VECTOR_TYPE_BINDING(VTYPE, NAME)\
+  pybind11::class_<VTYPE>(m, NAME)\
+      .def(pybind11::init<>())\
+      .def("clear", &VTYPE::clear)\
+      .def("pop_back", &VTYPE::pop_back)\
+      .def("__getitem__",\
+           [](const VTYPE& v, size_t i) {\
+             if (i >= v.size()) throw pybind11::index_error();\
+             return v[i];\
+           })\
+      .def("__setitem__",\
+           [](VTYPE& v, size_t i, VTYPE::value_type val) {\
+             if (i >= v.size()) throw pybind11::index_error();\
+             v[i] = val;\
+           })\
+      .def("__len__", [](const VTYPE& v) { return v.size(); })\
+      .def("__iter__", [](VTYPE& v) { return pybind11::make_iterator(v.begin(), v.end()); },\
+           pybind11::keep_alive<0, 1>()); /* Keep vector alive while iterator is used */
+
+// convenience macro to bind robot interface with all required vectors. LIB_NAME must match target name in CMakeLists
+#define CREATE_ROBOT_PYTHON_BINDINGS(PY_INTERFACE, LIB_NAME)\
+  /* make vector types opaque so they are not converted to python lists */\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::scalar_array_t);\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::state_vector_array_t);\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::input_vector_array_t);\
+  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::state_matrix_array_t);\
+  /* create a python module */\
+  PYBIND11_MODULE(LIB_NAME, m) {\
+    /* bind vector types so they can be used natively in python */\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::scalar_array_t, "scalar_array")\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::state_vector_array_t, "state_vector_array")\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::input_vector_array_t, "input_vector_array_t")\
+    VECTOR_TYPE_BINDING(PY_INTERFACE::state_matrix_array_t, "state_matrix_array_t")\
+    /* bind the actual mpc interface */\
+    pybind11::class_<PY_INTERFACE>(m, "mpc_interface")\
+        .def(pybind11::init<const std::string&, bool>())\
+        .def_property_readonly_static("STATE_DIM", [](pybind11::object) {return static_cast<int>(PY_INTERFACE::dim_t::DIMS::STATE_DIM_);})\
+        .def_property_readonly_static("INPUT_DIM", [](pybind11::object) {return static_cast<int>(PY_INTERFACE::dim_t::DIMS::INPUT_DIM_);})\
+        .def("setObservation", &PY_INTERFACE::setObservation, "t"_a, "x"_a.noconvert())\
+        .def("advanceMpc", &PY_INTERFACE::advanceMpc)\
+        .def("getMpcSolution", &PY_INTERFACE::getMpcSolution, "t"_a.noconvert(), "x"_a.noconvert(), "u"_a.noconvert(), "sigmaX"_a.noconvert())\
+        .def("computeFlowMap", &PY_INTERFACE::computeFlowMap, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("setFlowMapDerivativeStateAndControl", &PY_INTERFACE::setFlowMapDerivativeStateAndControl, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("computeFlowMapDerivativeState", &PY_INTERFACE::computeFlowMapDerivativeState)\
+        .def("computeFlowMapDerivativeInput", &PY_INTERFACE::computeFlowMapDerivativeInput)\
+        .def("getRunningCost", &PY_INTERFACE::getRunningCost, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("getRunningCostDerivativeState", &PY_INTERFACE::getRunningCostDerivativeState, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("getRunningCostDerivativeInput", &PY_INTERFACE::getRunningCostDerivativeInput, "t"_a, "x"_a.noconvert(), "u"_a.noconvert())\
+        .def("getValueFunctionStateDerivative", &PY_INTERFACE::getValueFunctionStateDerivative, "t"_a, "x"_a.noconvert());\
+    }

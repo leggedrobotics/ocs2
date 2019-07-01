69372d8c7c4c940e27b06c84c3c1ab145fb2df6f
Jan Carius <jan.carius@gmx.de>
2019-07-01 13:35:27 +0200

    hide input vector for anymal

--- ocs2_comm_interfaces/include/ocs2_comm_interfaces/ocs2_interfaces/Pybind_Macros.h
+++ ocs2_comm_interfaces/include/ocs2_comm_interfaces/ocs2_interfaces/Pybind_Macros.h
@@ -28,11 +28,12 @@ using namespace pybind11::literals;
            pybind11::keep_alive<0, 1>()); /* Keep vector alive while iterator is used */
 
 // convenience macro to bind robot interface with all required vectors. LIB_NAME must match target name in CMakeLists
+//@todo: redefinition error if state and input dim are the same -- solve this
 #define CREATE_ROBOT_PYTHON_BINDINGS(PY_INTERFACE, LIB_NAME)\
   /* make vector types opaque so they are not converted to python lists */\
   PYBIND11_MAKE_OPAQUE(PY_INTERFACE::scalar_array_t);\
   PYBIND11_MAKE_OPAQUE(PY_INTERFACE::state_vector_array_t);\
-  PYBIND11_MAKE_OPAQUE(PY_INTERFACE::input_vector_array_t);\
+  /*PYBIND11_MAKE_OPAQUE(PY_INTERFACE::input_vector_array_t);*/\
   PYBIND11_MAKE_OPAQUE(PY_INTERFACE::state_matrix_array_t);\
   PYBIND11_MAKE_OPAQUE(PY_INTERFACE::dynamic_vector_array_t);\
   /* create a python module */\
@@ -40,7 +41,7 @@ using namespace pybind11::literals;
     /* bind vector types so they can be used natively in python */\
     VECTOR_TYPE_BINDING(PY_INTERFACE::scalar_array_t, "scalar_array")\
     VECTOR_TYPE_BINDING(PY_INTERFACE::state_vector_array_t, "state_vector_array")\
-    VECTOR_TYPE_BINDING(PY_INTERFACE::input_vector_array_t, "input_vector_array")\
+    /*VECTOR_TYPE_BINDING(PY_INTERFACE::input_vector_array_t, "input_vector_array")*/\
     VECTOR_TYPE_BINDING(PY_INTERFACE::state_matrix_array_t, "state_matrix_array")\
     VECTOR_TYPE_BINDING(PY_INTERFACE::dynamic_vector_array_t, "dynamic_vector_array")\
     /* bind cost desired trajectories class */\

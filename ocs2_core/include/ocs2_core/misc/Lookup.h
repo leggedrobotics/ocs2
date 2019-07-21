23580a1ec0bb3248fd8e7a064d90f10ae111c79c
rgrandia <rubengrandia@gmail.com>
2019-07-21 10:53:41 +0200

    fix eventTime indexing in riccati integration

--- ocs2_core/include/ocs2_core/misc/Lookup.h
+++ ocs2_core/include/ocs2_core/misc/Lookup.h
@@ -5,8 +5,8 @@
 #ifndef OCS2_CTRL_LOOKUP_H
 #define OCS2_CTRL_LOOKUP_H
 
+#include <sstream>
 #include <vector>
-#include <complex>
 
 #include "ocs2_core/OCS2NumericTraits.h"
 
@@ -14,34 +14,32 @@ namespace ocs2 {
 namespace Lookup {
 
 /**
- * finds the index of an element in a sorted dataArray which is equal to value (epsilone distance)
+ * finds the index of an element in a sorted dataArray which is equal to value (epsilon distance)
  * @param [in] dataArray: data array
  * @param [in] value: enquiry value
  * @return: index
  */
 template <typename scalar_t = double>
-size_t findFirstIndexWithinTol(
-		const std::vector<scalar_t> &dataArray,
-		const scalar_t &value,
-		const scalar_t eps = OCS2NumericTraits<scalar_t>::weakEpsilon()) {
-
-  size_t index = dataArray.size();
-
-  for (size_t i = 0; i < dataArray.size(); i++){
+size_t findFirstIndexWithinTol(const std::vector<scalar_t>& dataArray, scalar_t value,
+                               scalar_t eps = OCS2NumericTraits<scalar_t>::weakEpsilon()) {
+  // Search for a match by linearly traversing the data, returning first match
+  for (size_t i = 0; i < dataArray.size(); i++) {
     if (std::abs(dataArray[i] - value) < eps) {
-      index = i;
-      break;
+      return i;
     }
   }
 
-  if (index == dataArray.size()) {
-    throw std::runtime_error("[findIndexOfClosestWithTol] Value not found within tolerance");
+  // If we reach here, no match was found
+  std::stringstream msg;
+  msg << "[findFirstIndexWithinTol] Value not found within tolerance, with \n\t dataArray: ";
+  for (const auto d : dataArray) {
+    msg << d << ", ";
   }
-
-  return index;
+  msg << "\n\t value: " << value << "\n\t epsilon: " << eps << std::endl;
+  throw std::runtime_error(msg.str());
 }
 
-} // namespace Lookup
-} // namespace ocs2
+}  // namespace Lookup
+}  // namespace ocs2
 
-#endif //OCS2_CTRL_LOOKUP_H
+#endif  // OCS2_CTRL_LOOKUP_H

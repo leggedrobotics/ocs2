67a56a682d9e1cbc84da2badb9ce9c36b8dc1b81
Jan Carius <jan.carius@gmx.de>
2019-08-01 16:03:27 +0200

    make toCostDesiredTrajectories return by value

--- ocs2_robotic_examples/ocs2_robotic_tools/include/ocs2_robotic_tools/command/TargetTrajectories_Keyboard_Interface.h
+++ ocs2_robotic_examples/ocs2_robotic_tools/include/ocs2_robotic_tools/command/TargetTrajectories_Keyboard_Interface.h
@@ -30,13 +30,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef TARGETTRAJECTORIES_KEYBOARD_INTERFACE_OCS2_H_
 #define TARGETTRAJECTORIES_KEYBOARD_INTERFACE_OCS2_H_
 
-#include <vector>
-#include <csignal>
-#include <iostream>
-#include <iomanip>
-#include <string>
 #include <Eigen/Core>
+#include <csignal>
+#include <iomanip>
+#include <iostream>
+#include <string>
 #include <thread>
+#include <vector>
 
 #include <ocs2_comm_interfaces/ocs2_ros_interfaces/command/TargetTrajectories_ROS_Interface.h>
 
@@ -48,102 +48,90 @@ namespace ocs2 {
  * @tparam SCALAR_T: scalar type.
  */
 template <typename SCALAR_T>
-class TargetTrajectories_Keyboard_Interface : public ocs2::TargetTrajectories_ROS_Interface<SCALAR_T>
-{
-public:
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+class TargetTrajectories_Keyboard_Interface : public ocs2::TargetTrajectories_ROS_Interface<SCALAR_T> {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	using BASE = ocs2::TargetTrajectories_ROS_Interface<SCALAR_T>;
-	using scalar_t = typename BASE::scalar_t;
-	using scalar_array_t = typename BASE::scalar_array_t;
-	using dynamic_vector_t = typename BASE::dynamic_vector_t;
-	using dynamic_vector_array_t = typename BASE::dynamic_vector_array_t;
-	using cost_desired_trajectories_t = typename BASE::cost_desired_trajectories_t;
+  using BASE = ocs2::TargetTrajectories_ROS_Interface<SCALAR_T>;
+  using scalar_t = typename BASE::scalar_t;
+  using scalar_array_t = typename BASE::scalar_array_t;
+  using dynamic_vector_t = typename BASE::dynamic_vector_t;
+  using dynamic_vector_array_t = typename BASE::dynamic_vector_array_t;
+  using cost_desired_trajectories_t = typename BASE::cost_desired_trajectories_t;
 
-	/**
-	 * Constructor
-	 *
-	 * @param [in] argc: Number of command line arguments
-	 * @param [in] argv: Command line arguments
-	 * @param [in] targetCommandSize: command expected length
-	 * @param [in] robotName: robot's name.
-	 * @param [in] targetCommandSize: command expected length
-	 * @param [in] targetCommandLimits: The limits of the loaded command from
-	 * command-line (for safety purposes).
-	 */
-	TargetTrajectories_Keyboard_Interface(
-			int argc,
-			char* argv[],
-	    const std::string& robotName = "robot",
-			const size_t targetCommandSize = 0,
-			const scalar_array_t& targetCommandLimits = scalar_array_t());
+  /**
+   * Constructor
+   *
+   * @param [in] argc: Number of command line arguments
+   * @param [in] argv: Command line arguments
+   * @param [in] targetCommandSize: command expected length
+   * @param [in] robotName: robot's name.
+   * @param [in] targetCommandSize: command expected length
+   * @param [in] targetCommandLimits: The limits of the loaded command from
+   * command-line (for safety purposes).
+   */
+  TargetTrajectories_Keyboard_Interface(int argc, char* argv[], const std::string& robotName = "robot", const size_t targetCommandSize = 0,
+                                        const scalar_array_t& targetCommandLimits = scalar_array_t());
 
-	/**
-	 * Default destructor
-	 */
-	virtual ~TargetTrajectories_Keyboard_Interface() = default;
+  /**
+   * Default destructor
+   */
+  virtual ~TargetTrajectories_Keyboard_Interface() = default;
 
-	/**
-	 * Gets the command vector size.
-	 *
-	 * @return The command vector size.
-	 */
-	size_t& targetCommandSize();
+  /**
+   * Gets the command vector size.
+   *
+   * @return The command vector size.
+   */
+  size_t& targetCommandSize();
 
-	/**
-	 * From command line loaded command to desired time, state, and input.
-	 * This method should be overridden by the user if modification is
-	 * required on the loaded command from command-line.
-	 *
-	 * @param [out] commadLineTarget: The loaded command target.
-	 * @param [in] desiredTime: Desired time to be published.
-	 * @param [in] desiredState: Desired state to be published.
-	 * @param [in] desiredInput: Desired input to be published.
-	 */
-	virtual void toCostDesiredTimeStateInput(
-			const scalar_array_t& commadLineTarget,
-			scalar_t& desiredTime,
-			dynamic_vector_t& desiredState,
-			dynamic_vector_t& desiredInput);
+  /**
+   * From command line loaded command to desired time, state, and input.
+   * This method should be overridden by the user if modification is
+   * required on the loaded command from command-line.
+   *
+   * @param [out] commadLineTarget: The loaded command target.
+   * @param [in] desiredTime: Desired time to be published.
+   * @param [in] desiredState: Desired state to be published.
+   * @param [in] desiredInput: Desired input to be published.
+   */
+  virtual void toCostDesiredTimeStateInput(const scalar_array_t& commadLineTarget, scalar_t& desiredTime, dynamic_vector_t& desiredState,
+                                           dynamic_vector_t& desiredInput);
 
-	/**
-	 * @brief Turns command line values into a costDesiredTrajectories object
-	 * @note Default implementation calls toCostDesiredTimeStateInput
-	 * @param[in] commadLineTarget
-	 * @param[out] costDesiredTrajectories
-	 */
-	virtual void toCostDesiredTrajectories(
-	    const scalar_array_t& commadLineTarget,
-	    cost_desired_trajectories_t& costDesiredTrajectories);
+  /**
+   * @brief Turns command line values into a costDesiredTrajectories object
+   * @note Default implementation calls toCostDesiredTimeStateInput
+   * @param[in] commadLineTarget
+   * @return costDesiredTrajectories
+   */
+  virtual cost_desired_trajectories_t toCostDesiredTrajectories(const scalar_array_t& commadLineTarget);
 
-	/**
-	 * Gets command line input. If the input command is shorter than the expected command
-	 * size (targetCommandSize_), the method will set the rest of the command to zero.
-	 *
-	 * @param [in] commadMsg: Message to be displayed on screen.
-	 */
-	void getKeyboardCommand(
-			const std::string& commadMsg = "Enter command, separated by spaces");
+  /**
+   * Gets command line input. If the input command is shorter than the expected command
+   * size (targetCommandSize_), the method will set the rest of the command to zero.
+   *
+   * @param [in] commadMsg: Message to be displayed on screen.
+   */
+  void getKeyboardCommand(const std::string& commadMsg = "Enter command, separated by spaces");
 
-protected:
-	/**
-	 * Gets the target from command line.
-	 *
-	 * @return targetCommand: The target command.
-	 */
-	scalar_array_t getCommandLine();
+ protected:
+  /**
+   * Gets the target from command line.
+   *
+   * @return targetCommand: The target command.
+   */
+  scalar_array_t getCommandLine();
 
+  /******
+   * Variables
+   ******/
+  size_t targetCommandSize_;
+  scalar_array_t targetCommandLimits_;
 
-/******
- * Variables
- ******/
-	size_t targetCommandSize_;
-	scalar_array_t targetCommandLimits_;
-
-	scalar_array_t targetCommand_;
+  scalar_array_t targetCommand_;
 };
 
-} // namespace ocs2
+}  // namespace ocs2
 
 #include "implementation/TargetTrajectories_Keyboard_Interface.h"
 

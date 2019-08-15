dd79ed656f0ab583f29d2df502f9c742d55f2fbb
Jan Carius <jan.carius@gmx.de>
2019-08-15 15:28:17 +0200

    update anymal python bindings

--- src/pyBindModule.cpp
+++ src/pyBindModule.cpp
@@ -1,4 +1,4 @@
 #include <ocs2_anymal_interface/AnymalPyBindings.h>
 #include <ocs2_comm_interfaces/ocs2_interfaces/Pybind_Macros.h>
 
-CREATE_ROBOT_PYTHON_BINDINGS(anymal::AnymalPyBindings, AnymalPyBindings3)
+CREATE_ROBOT_PYTHON_BINDINGS(anymal::AnymalPyBindings, AnymalPyBindings)

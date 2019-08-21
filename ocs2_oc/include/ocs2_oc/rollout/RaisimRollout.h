0cfc64b509b24c3ef34d5872299005fc55db9096
Jan Carius <jan.carius@gmx.de>
2019-08-21 21:53:15 +0200

    finished raisim rollout

--- ocs2_oc/include/ocs2_oc/rollout/RaisimRollout.h
+++ ocs2_oc/include/ocs2_oc/rollout/RaisimRollout.h
@@ -12,7 +12,7 @@ namespace ocs2 {
  * @tparam INPUT_DIM: Dimension of the control input space.
  */
 template <size_t STATE_DIM, size_t INPUT_DIM>
-class RaisimRollout : public RolloutBase<STATE_DIM, INPUT_DIM> {
+class RaisimRollout final : public RolloutBase<STATE_DIM, INPUT_DIM> {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -29,8 +29,28 @@ class RaisimRollout : public RolloutBase<STATE_DIM, INPUT_DIM> {
 
   using logic_rules_machine_t = HybridLogicRulesMachine;
 
-  explicit RaisimRollout(std::string pathToUrdf, Rollout_Settings rolloutSettings = Rollout_Settings(), char algorithmName[] = nullptr)
-      : Base(std::move(rolloutSettings), std::move(algorithmName)) {
+  using state_to_raisim_gen_coord_gen_vel_t = std::function<std::pair<Eigen::VectorXd, Eigen::VectorXd>(state_vector_t)>;
+  using raisim_gen_coord_gen_vel_to_state_t = std::function<state_vector_t(const Eigen::VectorXd&, const Eigen::VectorXd&)>;
+  using input_to_raisim_generalized_force_t = std::function<Eigen::VectorXd(const input_vector_t&, const state_vector_t&)>;
+
+  /**
+   * @brief Constructor
+   * @param pathToUrdf: Full file path to the urdf description for initializing the simulator
+   * @param stateToRaisimGenCoordGenVel: Transformation function that converts ocs2 state to generalized coordinate and generalized velocity
+   * used by RAIsim
+   * @param raisimGenCoordGenVelToState: Transformation function that converts RAIsim generalized coordinates and velocities to ocs2 state
+   * @param inputToRaisimGeneralizedForce: Tranformation function that converts ocs2 control input to RAIsim generalized force
+   * @param rolloutSettings
+   * @param algorithmName
+   */
+  RaisimRollout(const std::string& pathToUrdf, state_to_raisim_gen_coord_gen_vel_t stateToRaisimGenCoordGenVel,
+                raisim_gen_coord_gen_vel_to_state_t raisimGenCoordGenVelToState,
+                input_to_raisim_generalized_force_t inputToRaisimGeneralizedForce, Rollout_Settings rolloutSettings = Rollout_Settings(),
+                char algorithmName[] = nullptr)
+      : Base(std::move(rolloutSettings), std::move(algorithmName)),
+        stateToRaisimGenCoordGenVel_(std::move(stateToRaisimGenCoordGenVel)),
+        raisimGenCoordGenVelToState_(std::move(raisimGenCoordGenVelToState)),
+        inputToRaisimGeneralizedForce_(std::move(inputToRaisimGeneralizedForce)) {
     system_ = world_.addArticulatedSystem(pathToUrdf);
     ground_ = world_.addGround();
     world_.setTimeStep(this->settings().minTimeStep_);
@@ -40,8 +60,6 @@ class RaisimRollout : public RolloutBase<STATE_DIM, INPUT_DIM> {
                      controller_t* controller, logic_rules_machine_t& logicRulesMachine, scalar_array_t& timeTrajectory,
                      size_array_t& eventsPastTheEndIndeces, state_vector_array_t& stateTrajectory,
                      input_vector_array_t& inputTrajectory) override {
-    std::cout << "RaisimRollout::run called at time " << initTime << std::endl;
-
     // Prepare arrays
     const auto numSteps = static_cast<int>(std::round((finalTime - initTime) / this->settings().minTimeStep_));
     timeTrajectory.clear();
@@ -52,34 +70,28 @@ class RaisimRollout : public RolloutBase<STATE_DIM, INPUT_DIM> {
     inputTrajectory.reserve(numSteps + 1);
 
     // Set inital state to simulation
-    //! todo: use general conversion function passed in by the user
-    std::cout << "RAI dof = " << system_->getDOF() << std::endl;
-    system_->setState(initState.template head<2>(), initState.template tail<2>());
+    Eigen::VectorXd q_init, dq_init;
+    std::tie(q_init, dq_init) = stateToRaisimGenCoordGenVel_(initState);
+    system_->setState(q_init, dq_init);
 
     // Forward simulate
     for (int i = 0; i < numSteps; i++) {
       world_.integrate1();  //! prepares all dynamical quantities for current time step
 
-      //! @todo extract required quantities here
-      Eigen::MatrixXd M = system_->getMassMatrix().e();
-      std::cout << "M\n" << M << std::endl;
+      //! @todo(jcarius) extract additional quantities (mass matrix, jacobians, contact points) here
+      // Eigen::MatrixXd M = system_->getMassMatrix().e();
 
       Eigen::VectorXd raisim_q, raisim_dq;
       system_->getState(raisim_q, raisim_dq);
 
       const auto time = initTime + i * this->settings().minTimeStep_;
       timeTrajectory.push_back(time);
-      stateTrajectory.emplace_back(state_vector_t());
-      stateTrajectory.back() << raisim_q, raisim_dq;
-      std::cout << "state: " << stateTrajectory.back().transpose() << std::endl;
-      input_vector_t input = controller->computeInput(time, stateTrajectory.back());
-      std::cout << "input: " << input.transpose() << std::endl;
-      inputTrajectory.emplace_back(input);
 
-      Eigen::VectorXd tau(2);
-      tau.setZero();
-      tau.head(1) = inputTrajectory.back();
-      system_->setGeneralizedForce(tau);
+      stateTrajectory.emplace_back(raisimGenCoordGenVelToState_(raisim_q, raisim_dq));
+
+      input_vector_t input = controller->computeInput(time, stateTrajectory.back());
+      inputTrajectory.emplace_back(input);
+      system_->setGeneralizedForce(inputToRaisimGeneralizedForce_(input, stateTrajectory.back()));
 
       world_.integrate2();
     }
@@ -87,20 +99,25 @@ class RaisimRollout : public RolloutBase<STATE_DIM, INPUT_DIM> {
 
     // also push back final state and input
     timeTrajectory.push_back(initTime + numSteps * this->settings().minTimeStep_);
+
     Eigen::VectorXd raisim_q, raisim_dq;
     system_->getState(raisim_q, raisim_dq);
-    stateTrajectory.emplace_back(state_vector_t());
-    stateTrajectory.back() << raisim_q, raisim_dq;
-    std::cout << "state: " << stateTrajectory.back().transpose() << std::endl;
+    stateTrajectory.emplace_back(raisimGenCoordGenVelToState_(raisim_q, raisim_dq));
+
     input_vector_t input = controller->computeInput(timeTrajectory.back(), stateTrajectory.back());
-    std::cout << "input: " << input.transpose() << std::endl;
     inputTrajectory.emplace_back(input);
   }
 
  protected:
+  // Handles to RAIsim objects
   raisim::World world_;
   raisim::Ground* ground_;
   raisim::ArticulatedSystem* system_;
+
+  // Robot-specific conversions
+  state_to_raisim_gen_coord_gen_vel_t stateToRaisimGenCoordGenVel_;
+  raisim_gen_coord_gen_vel_to_state_t raisimGenCoordGenVelToState_;
+  input_to_raisim_generalized_force_t inputToRaisimGeneralizedForce_;
 };
 
 }  // namespace ocs2
--- ocs2_robotic_examples/ocs2_cart_pole_example/src/Dummy_Cartpole_Raisim_Node.cpp
+++ ocs2_robotic_examples/ocs2_cart_pole_example/src/Dummy_Cartpole_Raisim_Node.cpp
@@ -1,21 +1,59 @@
 #include <ros/package.h>
 
-#include <ocs2_oc/rollout/RaisimRollout.h>
 #include <ocs2_cart_pole_example/CartPoleInterface.h>
 #include <ocs2_cart_pole_example/definitions.h>
 #include <ocs2_cart_pole_example/ros_comm/MRT_ROS_Cartpole.h>
 #include <ocs2_cart_pole_example/ros_comm/MRT_ROS_Dummy_Cartpole.h>
+#include <ocs2_oc/rollout/RaisimRollout.h>
+
+/**
+ * @brief Convert ocs2 cartpole state to generalized coordinate and generalized velocity used by RAIsim
+ * @param[in] state: the state to be converted
+ * @return {q, dq} pair that represents the state
+ */
+std::pair<Eigen::VectorXd, Eigen::VectorXd> stateToRaisimGenCoordGenVel(
+    const ocs2::Dimensions<ocs2::cartpole::STATE_DIM_, ocs2::cartpole::INPUT_DIM_>::state_vector_t& state) {
+  return {state.head<2>(), state.tail<2>()};
+}
+
+/**
+ * @brief Convert RAIsim generalized coordinates and velocities to ocs2 cartpole state
+ * @note This should be the inverse to stateToRaisimGenCoordGenVel
+ * @param[in] q the generalized coordinate
+ * @param[in] dq the generalized velocity
+ * @return the corresponding ocs2 cart pole state
+ */
+ocs2::Dimensions<ocs2::cartpole::STATE_DIM_, ocs2::cartpole::INPUT_DIM_>::state_vector_t raisimGenCoordGenVelToState(
+    const Eigen::VectorXd& q, const Eigen::VectorXd& dq) {
+  ocs2::Dimensions<ocs2::cartpole::STATE_DIM_, ocs2::cartpole::INPUT_DIM_>::state_vector_t state;
+  state << q, dq;
+  return state;
+}
+
+/**
+ * @brief Convert ocs2 control input to RAIsim generalized force
+ * @param[in] input: The control computed by the ocs2 controller
+ * @param[in] state: The current state
+ * @return The generalized forces to be applied to the system
+ */
+Eigen::VectorXd inputToRaisimGeneralizedForce(
+    const ocs2::Dimensions<ocs2::cartpole::STATE_DIM_, ocs2::cartpole::INPUT_DIM_>::input_vector_t& input,
+    const ocs2::Dimensions<ocs2::cartpole::STATE_DIM_, ocs2::cartpole::INPUT_DIM_>::state_vector_t& state) {
+  Eigen::VectorXd generalizedForce = Eigen::VectorXd::Zero(2);
+  generalizedForce.head(1) = input;
+  return generalizedForce;
+}
 
 int main(int argc, char* argv[]) {
   // task file
   if (argc <= 1) throw std::runtime_error("No task file specified. Aborting.");
   std::string taskFileFolderName = std::string(argv[1]);
 
-  std::cout << "Starting DUMMY" << std::endl;
-
   // setup MRT with simulator rollouts
   using sim_rollout_t = ocs2::RaisimRollout<ocs2::cartpole::STATE_DIM_, ocs2::cartpole::INPUT_DIM_>;
-  std::unique_ptr<sim_rollout_t> simRollout(new sim_rollout_t(ros::package::getPath("ocs2_cart_pole_example") + "/urdf/cartpole.urdf"));
+  std::unique_ptr<sim_rollout_t> simRollout(new sim_rollout_t(ros::package::getPath("ocs2_cart_pole_example") + "/urdf/cartpole.urdf",
+                                                              &stateToRaisimGenCoordGenVel, &raisimGenCoordGenVelToState,
+                                                              &inputToRaisimGeneralizedForce));
   ocs2::cartpole::MRT_ROS_Cartpole::Ptr mrtPtr(new ocs2::cartpole::MRT_ROS_Cartpole("cartpole"));
   mrtPtr->initRollout(std::move(simRollout));
 

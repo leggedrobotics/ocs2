080dcbecd669c88d40faf5093350c4b87b5515c9
Farbod Farshidian <farbod.farshidian@gmail.com>
2019-09-15 11:17:48 +0200

    constructors are added to PrimalSolution class

--- ocs2_oc/include/ocs2_oc/oc_data/PrimalSolution.h
+++ ocs2_oc/include/ocs2_oc/oc_data/PrimalSolution.h
@@ -46,13 +46,51 @@ template <size_t STATE_DIM, size_t INPUT_DIM>
 struct PrimalSolution {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+  using controller_t = ControllerBase<STATE_DIM, INPUT_DIM>;
   using dim_t = Dimensions<STATE_DIM, INPUT_DIM>;
   using size_array_t = typename dim_t::size_array_t;
   using scalar_array_t = typename dim_t::scalar_array_t;
   using state_vector_array_t = typename dim_t::state_vector_array_t;
   using input_vector_array_t = typename dim_t::input_vector_array_t;
 
-  using controller_t = ControllerBase<STATE_DIM, INPUT_DIM>;
+  /**
+   * Constructor
+   */
+  PrimalSolution() = default;
+
+  /**
+   * Destructor
+   */
+  ~PrimalSolution() = default;
+
+  /**
+   * Move constructor
+   */
+  PrimalSolution(PrimalSolution&& other) = default;
+
+  /**
+   * Copy constructor
+   */
+  PrimalSolution(const PrimalSolution& other)
+      : timeTrajectory_(other.timeTrajectory_),
+        stateTrajectory_(other.stateTrajectory_),
+        inputTrajectory_(other.inputTrajectory_),
+        eventTimes_(other.eventTimes_),
+        subsystemsSequence_(other.subsystemsSequence_),
+        controllerPtr_(other.controllerPtr_->clone()) {}
+
+  /**
+   * Assignment operators
+   */
+  PrimalSolution& operator=(PrimalSolution other) noexcept {
+    timeTrajectory_.swap(other.timeTrajectory_);
+    stateTrajectory_.swap(other.stateTrajectory_);
+    inputTrajectory_.swap(other.inputTrajectory_);
+    eventTimes_.swap(other.eventTimes_);
+    subsystemsSequence_.swap(other.subsystemsSequence_);
+    controllerPtr_.swap(other.controllerPtr_);
+    return *this;
+  }
 
   scalar_array_t timeTrajectory_;
   state_vector_array_t stateTrajectory_;

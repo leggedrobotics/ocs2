10c7bc479a60b09d25ffa2968e1842d50625c172
Farbod Farshidian <farbod.farshidian@gmail.com>
2019-09-18 15:37:53 +0200

    update

--- ocs2_robotic_examples/ocs2_quadrotor_example/src/Dummy_Quadrotor_Node.cpp
+++ ocs2_robotic_examples/ocs2_quadrotor_example/src/Dummy_Quadrotor_Node.cpp
@@ -27,6 +27,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
+#include <ocs2_comm_interfaces/ocs2_ros_interfaces/mrt/MRT_ROS_Interface.h>
 #include <ocs2_robotic_tools/command/TargetPoseTransformation.h>
 
 #include "ocs2_quadrotor_example/QuadrotorInterface.h"

f54edda25018ce95f2c63d6779c79576e765c798
rgrandia <rubengrandia@gmail.com>
2019-09-18 20:54:47 +0200

    use reset to initialize unique_ptrs

--- src/OCS2AnymalInterface.cpp
+++ src/OCS2AnymalInterface.cpp
@@ -25,26 +25,26 @@ OCS2AnymalInterface::OCS2AnymalInterface(const std::string& pathToConfigFolder)
 /******************************************************************************************************/
 void OCS2AnymalInterface::setupOptimizer(const logic_rules_ptr_t& logicRulesPtr, const mode_sequence_template_t* modeSequenceTemplatePtr,
                                          slq_base_ptr_t& slqPtr, mpc_ptr_t& mpcPtr) {
-  dynamicsPtr_ = std::unique_ptr<system_dynamics_t>(new system_dynamics_t(modelSettings_.recompileLibraries_));
+  dynamicsPtr_.reset(new system_dynamics_t(modelSettings_.recompileLibraries_));
   dynamicsDerivativesPtr_.reset(dynamicsPtr_->clone());
-  constraintsPtr_ = std::unique_ptr<constraint_t>(new constraint_t(logicRulesPtr, modelSettings_));
-  costFunctionPtr_ = std::unique_ptr<cost_function_t>(new cost_function_t(logicRulesPtr, Q_, R_, QFinal_));
+  constraintsPtr_.reset(new constraint_t(logicRulesPtr, modelSettings_));
+  costFunctionPtr_.reset(new cost_function_t(logicRulesPtr, Q_, R_, QFinal_));
 
   generalized_coordinate_t defaultCoordinate = initRbdState_.template head<18>();
-  operatingPointsPtr_ = std::unique_ptr<operating_point_t>(new operating_point_t(logicRulesPtr, modelSettings_, defaultCoordinate));
+  operatingPointsPtr_.reset(new operating_point_t(logicRulesPtr, modelSettings_, defaultCoordinate));
 
   // SLQ
   if (slqSettings_.ddpSettings_.useMultiThreading_) {
-    slqPtr = slq_base_ptr_t(new slq_mp_t(dynamicsPtr_.get(), dynamicsDerivativesPtr_.get(), constraintsPtr_.get(), costFunctionPtr_.get(),
+    slqPtr.reset(new slq_mp_t(dynamicsPtr_.get(), dynamicsDerivativesPtr_.get(), constraintsPtr_.get(), costFunctionPtr_.get(),
                                          operatingPointsPtr_.get(), slqSettings_, logicRulesPtr));
   } else {
-    slqPtr = slq_base_ptr_t(new slq_t(dynamicsPtr_.get(), dynamicsDerivativesPtr_.get(), constraintsPtr_.get(), costFunctionPtr_.get(),
+    slqPtr.reset(new slq_t(dynamicsPtr_.get(), dynamicsDerivativesPtr_.get(), constraintsPtr_.get(), costFunctionPtr_.get(),
                                       operatingPointsPtr_.get(), slqSettings_, logicRulesPtr));
   }
 
   // MPC
   if (!modelSettings_.gaitOptimization_) {
-    mpcPtr = mpc_ptr_t(new mpc_t(dynamicsPtr_.get(), dynamicsDerivativesPtr_.get(), constraintsPtr_.get(), costFunctionPtr_.get(),
+    mpcPtr.reset(new mpc_t(dynamicsPtr_.get(), dynamicsDerivativesPtr_.get(), constraintsPtr_.get(), costFunctionPtr_.get(),
                                  operatingPointsPtr_.get(), partitioningTimes_, slqSettings_, mpcSettings_, logicRulesPtr,
                                  modeSequenceTemplatePtr));
 

d9934f9b4c34932b6a5beffc0725613c1e9b6246
Jan Carius <jan.carius@gmx.de>
2019-09-25 14:30:55 +0200

    fix raisimRollout after merging, add unit test

--- ocs2_robotic_examples/ocs2_cart_pole_raisim_example/test/RolloutTest.cpp
+++ ocs2_robotic_examples/ocs2_cart_pole_raisim_example/test/RolloutTest.cpp
@@ -28,52 +28,76 @@ TEST(ocs2_cart_pole_raisim_example, ConversionsConsistency) {
 
   state_vector_t ocs2_state2 = ocs2::cartpole::raisimGenCoordGenVelToState(raisim_q, raisim_dq);
   bool test = ocs2_state.isApprox(ocs2_state2);
-  ASSERT_TRUE(test);
+  EXPECT_TRUE(test);
 
   Eigen::VectorXd raisim_q2, raisim_dq2;
   std::tie(raisim_q2, raisim_dq2) = ocs2::cartpole::stateToRaisimGenCoordGenVel(ocs2_state2, ocs2_input);
   test = raisim_q2.isApprox(raisim_q);
-  ASSERT_TRUE(test);
+  EXPECT_TRUE(test);
   test = raisim_dq2.isApprox(raisim_dq);
-  ASSERT_TRUE(test);
+  EXPECT_TRUE(test);
 }
 
 TEST(ocs2_cart_pole_raisim_example, RolloutTest) {
-  scalar_array_t controllerTime{0.0, 1.0};
-  input_vector_array_t controllerInput(2, input_vector_t::Zero());
-
+  // define zero-input controller
+  const scalar_array_t controllerTime{0.0, 1.0};
+  const input_vector_array_t controllerInput(2, input_vector_t::Zero());
   ocs2::FeedforwardController<STATE_DIM_, INPUT_DIM_> ctrl(controllerTime, controllerInput);
 
-  size_t partitionIndex = 0;
-  const double t0 = 0.0;
-  const double tf = 2.0;
+  const double t0 = 0.2;
+  const double tf = 2.2;
+  const scalar_array_t eventTimes{0.1, 0.5, 1.0, 1.0, 1.5, 2.5};
   state_vector_t x_init;
   x_init.setZero();
   x_init(0) = M_PI;  // pole position (0 corresponds to up)
   x_init(1) = 0.3;   // cart position
   x_init(3) = 5.0;   // initial cart velocity
 
-  state_vector_t finalState;
+  scalar_array_t timeTrajectory;
+  size_array_t eventsPastTheEndIndeces;
+  state_vector_array_t stateTrajectory;
+  input_vector_array_t inputTrajectory;
+
+  ocs2::RaisimRollout<STATE_DIM_, INPUT_DIM_> rollout(
+      ros::package::getPath("ocs2_cart_pole_example") + "/urdf/cartpole.urdf", &ocs2::cartpole::stateToRaisimGenCoordGenVel,
+      &ocs2::cartpole::raisimGenCoordGenVelToState, &ocs2::cartpole::inputToRaisimGeneralizedForce);
+  rollout.settings().minTimeStep_ = 0.2;
+
+  state_vector_t finalState =
+      rollout.run(t0, x_init, tf, &ctrl, eventTimes, timeTrajectory, eventsPastTheEndIndeces, stateTrajectory, inputTrajectory);
+
+  // check if initial state is what we expected
   {
-    ocs2::HybridLogicRulesMachine logicRulesMachine;
-    scalar_array_t timeTrajectory;
-    size_array_t eventsPastTheEndIndeces;
-    state_vector_array_t stateTrajectory;
-    input_vector_array_t inputTrajectory;
-
-    ocs2::RaisimRollout<STATE_DIM_, INPUT_DIM_> rollout(
-        ros::package::getPath("ocs2_cart_pole_example") + "/urdf/cartpole.urdf", &ocs2::cartpole::stateToRaisimGenCoordGenVel,
-        &ocs2::cartpole::raisimGenCoordGenVelToState, &ocs2::cartpole::inputToRaisimGeneralizedForce);
-
-    finalState = rollout.run(partitionIndex, t0, x_init, tf, &ctrl, logicRulesMachine, timeTrajectory, eventsPastTheEndIndeces,
-                             stateTrajectory, inputTrajectory);
+    bool test = x_init.isApprox(stateTrajectory.front());
+    EXPECT_TRUE(test);
   }
 
-  state_vector_t finalState_expected = x_init;
-  finalState_expected(1) += (tf - t0) * x_init(3);
+  // check if final state is consistent with return value
+  {
+    bool test = finalState.isApprox(stateTrajectory.back());
+    EXPECT_TRUE(test);
+  }
 
-  bool test = finalState.isApprox(finalState_expected);
-  ASSERT_TRUE(test);
+  // check if final state is what we expected
+  {
+    state_vector_t finalState_expected = x_init;
+    finalState_expected(1) += (tf - t0) * x_init(3);
+    bool test = finalState.isApprox(finalState_expected, 1e-7);
+    EXPECT_TRUE(test);
+  }
+
+  // check if event times have been treated correctly
+  {
+    const auto eventTimesBetweent0tf =
+        std::upper_bound(eventTimes.begin(), eventTimes.end(), tf) - std::upper_bound(eventTimes.begin(), eventTimes.end(), t0);
+    EXPECT_EQ(eventTimesBetweent0tf, eventsPastTheEndIndeces.size());
+
+    for (size_t i = 0; i < eventsPastTheEndIndeces.size(); i++) {
+      auto timeJustBeforeEvent = timeTrajectory[eventsPastTheEndIndeces[i] - 1];
+      auto eventTime = *(std::upper_bound(eventTimes.begin(), eventTimes.end(), t0) + i);
+      EXPECT_DOUBLE_EQ(eventTime, timeJustBeforeEvent);
+    }
+  }
 }
 
 int main(int argc, char** argv) {

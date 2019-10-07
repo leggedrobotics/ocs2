2540f6d51324cfdbbb07ae8dcb0873b643e770f7
Michael Spieler <mspieler@ethz.ch>
2019-10-07 10:35:27 +0200

    Test matrix flatten and unflatten functions

--- ocs2_ddp/ocs2_slq/test/testRiccatiEquations.cpp
+++ ocs2_ddp/ocs2_slq/test/testRiccatiEquations.cpp
@@ -101,7 +101,7 @@ TEST(testRiccatiEquations, compareImplementations) {
   riccatiEquationPrecompute.computeFlowMap(0.6, S, dSdz_precompute);
   riccatiEquationNoPrecompute.computeFlowMap(0.6, S, dSdz_noPrecompute);
 
-  ASSERT_LE((dSdz_precompute - dSdz_noPrecompute).array().abs().maxCoeff(), 1e-9);
+  EXPECT_LE((dSdz_precompute - dSdz_noPrecompute).array().abs().maxCoeff(), 1e-9);
 }
 
 TEST(testRiccatiEquations, compareFixedAndDynamicSizedImplementation) {
@@ -122,7 +122,7 @@ TEST(testRiccatiEquations, compareFixedAndDynamicSizedImplementation) {
   S_static.setRandom();
   riccati_static.computeFlowMap(0.6, S_static, dSdz_static);
 
-  using riccati_dynamic_t = ocs2::SequentialRiccatiEquationsNormalized<-1, -1>;
+  using riccati_dynamic_t = ocs2::SequentialRiccatiEquationsNormalized<Eigen::Dynamic, Eigen::Dynamic>;
   riccati_dynamic_t riccati_dynamic(makePSD, precompute);
   srand(42);
   RiccatiInitializer<riccati_dynamic_t> rid(state_dim, input_dim);
@@ -133,7 +133,83 @@ TEST(testRiccatiEquations, compareFixedAndDynamicSizedImplementation) {
   S = S_static;
   riccati_dynamic.computeFlowMap(0.6, S, dSdz);
 
-  ASSERT_LE((dSdz - dSdz_static).array().abs().maxCoeff(), 1e-9);
+  EXPECT_LE((dSdz - dSdz_static).array().abs().maxCoeff(), 1e-9);
+}
+
+TEST(testRiccatiEquations, testFlattenSMatrix) {
+  const int state_dim = 4;
+  using riccati_t = ocs2::SequentialRiccatiEquationsNormalized<Eigen::Dynamic, Eigen::Dynamic>;
+
+  riccati_t::s_vector_t allSs, allSs_expect;
+  riccati_t::state_matrix_t Sm;
+  riccati_t::state_vector_t Sv;
+  riccati_t::eigen_scalar_t s;
+
+  Sm.resize(state_dim, state_dim);
+  Sm << 1, 2, 4, 7,  // clang-format off
+        2, 3, 5, 8,
+        4, 5, 6, 9,
+        7, 8, 9, 10;  // clang-format on
+  Sv.resize(state_dim);
+  Sv << 11, 12, 13, 14;
+  s << 15;
+
+  allSs.resize(15);
+  allSs_expect.resize(15);
+  allSs_expect << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15;
+
+  riccati_t::convert2Vector(Sm, Sv, s, allSs);
+
+  EXPECT_EQ(allSs, allSs_expect);
+}
+
+TEST(testRiccatiEquations, testFlattenAndUnflattenSMatrix) {
+  const int input_dim = 10;
+  const int state_dim = 48;
+  using riccati_t = ocs2::SequentialRiccatiEquationsNormalized<state_dim, input_dim>;
+
+  riccati_t::s_vector_t allSs;
+  riccati_t::state_matrix_t Sm, Sm_out;
+  riccati_t::state_vector_t Sv, Sv_out;
+  riccati_t::eigen_scalar_t s, s_out;
+
+  Sm.setRandom();
+  Sm = (Sm + Sm.transpose()).eval();
+  Sv.setRandom();
+  s.setRandom();
+
+  riccati_t::convert2Vector(Sm, Sv, s, allSs);
+  riccati_t::convert2Matrix(allSs, Sm_out, Sv_out, s_out);
+
+  EXPECT_EQ(Sm, Sm_out);
+  EXPECT_EQ(Sv, Sv_out);
+  EXPECT_EQ(s, s_out);
+}
+
+TEST(testRiccatiEquations, testFlattenAndUnflattenSMatrixDynamic) {
+  const int state_dim = 42;
+  using riccati_t = ocs2::SequentialRiccatiEquationsNormalized<Eigen::Dynamic, Eigen::Dynamic>;
+
+  riccati_t::s_vector_t allSs;
+  riccati_t::state_matrix_t Sm, Sm_out;
+  riccati_t::state_vector_t Sv, Sv_out;
+  riccati_t::eigen_scalar_t s, s_out;
+
+  Sm.setRandom(state_dim, state_dim);
+  Sm = (Sm + Sm.transpose()).eval();
+  Sv.setRandom(state_dim);
+  s.setRandom();
+
+  Sm_out.setZero(state_dim, state_dim);
+  Sv_out.setZero(state_dim);
+  s_out.setZero();
+
+  riccati_t::convert2Vector(Sm, Sv, s, allSs);
+  riccati_t::convert2Matrix(allSs, Sm_out, Sv_out, s_out);
+
+  EXPECT_EQ(Sm, Sm_out);
+  EXPECT_EQ(Sv, Sv_out);
+  EXPECT_EQ(s, s_out);
 }
 
 int main(int argc, char** argv) {

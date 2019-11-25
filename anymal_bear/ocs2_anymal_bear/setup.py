3a08910cc188dad10fc6bc16506f86350fbfad59
Jan Carius <jan.carius@gmx.de>
2019-11-25 11:00:06 +0100

    fix python bindings for ocs2_anymal_bear after renaming

--- anymal_bear/ocs2_anymal_bear/setup.py
+++ anymal_bear/ocs2_anymal_bear/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['ocs2_anymal_bear_interface'],
+     packages=['ocs2_anymal_bear'],
      package_dir={'': 'src'}
 )
 

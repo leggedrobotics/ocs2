01a2d9a187f984d1c7e5c0b1397356b4a36bdd80
Oliver Harley <Oliver.r.haley@gmail.com>
2019-11-26 20:43:43 +0100

    common virtual EndEffector constraint class
    There was quite a bit of code duplication

---
+++ common/ocs2_switched_model_interface/include/ocs2_switched_model_interface/constraint/EndEffectorConstraint.h
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <ocs2_core/automatic_differentiation/CppAdInterface.h>
+#include <ocs2_switched_model_interface/constraint/ConstraintTerm.h>
+
+#include <ocs2_switched_model_interface/core/SwitchedModel.h>
+#include "ocs2_switched_model_interface/core/ComModelBase.h"
+#include "ocs2_switched_model_interface/core/KinematicsModelBase.h"
+
+namespace switched_model {
+
+struct EndEffectorConstraintSettings {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  // Constraints are  A * v_base + b
+  Eigen::MatrixXd A;
+  Eigen::VectorXd b;
+};
+
+class EndEffectorConstraint : public ocs2::ConstraintTerm<STATE_DIM, INPUT_DIM> {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  using BASE = ocs2::ConstraintTerm<STATE_DIM, INPUT_DIM>;
+  using typename BASE::input_matrix_t;
+  using typename BASE::input_state_matrix_t;
+  using typename BASE::input_vector_t;
+  using typename BASE::LinearApproximation_t;
+  using typename BASE::QuadraticApproximation_t;
+  using typename BASE::scalar_array_t;
+  using typename BASE::scalar_t;
+  using typename BASE::state_matrix_t;
+  using typename BASE::state_vector_t;
+
+ protected:
+  static constexpr char const* libFolderDir = "/tmp/ocs2";
+  static constexpr size_t domain_dim_ = 1 + STATE_DIM + INPUT_DIM;
+  static constexpr size_t range_dim_ = 3;
+
+ public:
+  using ad_interface_t = ocs2::CppAdInterface<scalar_t>;
+  using ad_scalar_t = typename ad_interface_t::ad_scalar_t;
+  using ad_dynamic_vector_t = typename ad_interface_t::ad_dynamic_vector_t;
+  using dynamic_vector_t = typename ad_interface_t::dynamic_vector_t;
+
+  using constraint_timeStateInput_matrix_t = Eigen::Matrix<scalar_t, -1, 1 + STATE_DIM + INPUT_DIM>;
+  using timeStateInput_matrix_t = Eigen::Matrix<scalar_t, 1 + STATE_DIM + INPUT_DIM, 1 + STATE_DIM + INPUT_DIM>;
+  using ad_com_model_t = ComModelBase<ad_scalar_t>;
+  using ad_kinematic_model_t = KinematicsModelBase<ad_scalar_t>;
+
+  EndEffectorConstraint(ocs2::ConstraintOrder constraintOrder, std::string eeConstraintName, int legNumber,
+                        EndEffectorConstraintSettings settings, ad_com_model_t& adComModel, ad_kinematic_model_t& adKinematicsModel,
+                        bool generateModels)
+      : BASE(constraintOrder),
+        legNumber_(legNumber),
+        settings_(std::move(settings)),
+        libName_(eeConstraintName + std::to_string(legNumber_)),
+        libFolder_(libFolderDir) {
+    setAdInterface(adComModel, adKinematicsModel);
+    switch (constraintOrder) {
+      case ocs2::ConstraintOrder::None:
+        // TODO(oharley) unsure what we put here?
+        // if (generateModels) {
+        //   // adInterface_->createModels(ad_interface_t::ApproximationOrder::Zero, true);
+        // } else {
+        //
+        //   adInterface_->loadModelsIfAvailable(ad_interface_t::ApproximationOrder::Zero, true);
+        // }
+        // break;
+      case ocs2::ConstraintOrder::Linear:
+        if (generateModels) {
+          adInterface_->createModels(ad_interface_t::ApproximationOrder::First, true);
+        } else {
+          adInterface_->loadModelsIfAvailable(ad_interface_t::ApproximationOrder::First, true);
+        }
+        break;
+      case ocs2::ConstraintOrder::Quadratic:
+        if (generateModels) {
+          adInterface_->createModels(ad_interface_t::ApproximationOrder::Second, true);
+        } else {
+          adInterface_->loadModelsIfAvailable(ad_interface_t::ApproximationOrder::Second, true);
+        }
+        break;
+    }
+  }
+
+  EndEffectorConstraint(const EndEffectorConstraint& rhs)
+      : BASE(rhs),
+        legNumber_(rhs.legNumber_),
+        settings_(rhs.settings_),
+        libName_(rhs.libName_),
+        libFolder_(rhs.libFolder_),
+        adInterface_(new ad_interface_t(*rhs.adInterface_)) {}
+
+  // virtual EndEffectorConstraint* clone() const override { return new EndEffectorConstraint(*this); }
+
+  virtual void configure(const EndEffectorConstraintSettings& settings) { settings_ = settings; };
+
+  size_t getNumConstraints(scalar_t time) const override { return settings_.A.rows(); };
+
+  // virtual scalar_array_t getValue(scalar_t time, const state_vector_t& state, const input_vector_t& input) const = 0;
+
+  virtual LinearApproximation_t getLinearApproximation(scalar_t time, const state_vector_t& state,
+                                                       const input_vector_t& input) const override {
+    // Assemble input
+    dynamic_vector_t tapedInput(1 + STATE_DIM + INPUT_DIM);
+    tapedInput << time, state, input;
+
+    // Compute end effector velocity and derivatives
+    constraint_timeStateInput_matrix_t stateInputJacobian = adInterface_->getJacobian(tapedInput);
+
+    // Collect constraint terms
+    auto dhdx = stateInputJacobian.template middleCols<STATE_DIM>(1);
+    auto dhdu = stateInputJacobian.template rightCols<INPUT_DIM>();
+
+    // Convert to output format
+    LinearApproximation_t linearApproximation;
+    linearApproximation.constraintValues = getValue(time, state, input);
+    for (int i = 0; i < settings_.A.rows(); i++) {
+      linearApproximation.derivativeState.emplace_back(settings_.A.row(i) * dhdx);
+      linearApproximation.derivativeInput.emplace_back(settings_.A.row(i) * dhdu);
+    }
+    return linearApproximation;
+  }
+
+  virtual QuadraticApproximation_t getQuadraticApproximation(scalar_t time, const state_vector_t& state,
+                                                             const input_vector_t& input) const override {
+    // Assemble input
+    dynamic_vector_t tapedInput(1 + STATE_DIM + INPUT_DIM);
+    tapedInput << time, state, input;
+
+    // Convert to output format
+    QuadraticApproximation_t quadraticApproximation;
+    auto linearApproximation = getLinearApproximation(time, state, input);
+    quadraticApproximation.constraintValues = std::move(linearApproximation.constraintValues);
+    quadraticApproximation.derivativeState = std::move(linearApproximation.derivativeState);
+    quadraticApproximation.derivativeInput = std::move(linearApproximation.derivativeInput);
+    for (int i = 0; i < settings_.A.rows(); i++) {
+      timeStateInput_matrix_t weightedHessian = adInterface_->getHessian(settings_.A.row(i), tapedInput);
+
+      quadraticApproximation.secondDerivativesState.emplace_back(weightedHessian.block(1, 1, STATE_DIM, STATE_DIM));
+      quadraticApproximation.secondDerivativesInput.emplace_back(weightedHessian.block(1 + STATE_DIM, 1 + STATE_DIM, INPUT_DIM, INPUT_DIM));
+      quadraticApproximation.derivativesInputState.emplace_back(weightedHessian.block(1 + STATE_DIM, 1, INPUT_DIM, STATE_DIM));
+    }
+    return quadraticApproximation;
+  }
+
+ private:
+  virtual void setAdInterface(ad_com_model_t& adComModel, ad_kinematic_model_t& adKinematicsModel){
+      /*
+       *   // Function to differentiate
+       * auto adfunc = [this, &adComModel, &adKinematicsModel](const ad_dynamic_vector_t& x, ad_dynamic_vector_t& y) {
+       *     this->adFootVelocity(adComModel, adKinematicsModel, x, y);
+       * };
+       *
+       *   adInterface_.reset(new ad_interface_t(adfunc, BASE::range_dim_, BASE::domain_dim_, libName_, libFolder_));
+       * };
+       */
+  };
+
+ protected:
+  int legNumber_;
+  EndEffectorConstraintSettings settings_;
+  std::string libName_;
+  std::string libFolder_;
+  std::unique_ptr<ad_interface_t> adInterface_;
+};
+}  // namespace switched_model
--- common/ocs2_switched_model_interface/include/ocs2_switched_model_interface/constraint/EndEffectorPositionConstraint.h
+++ common/ocs2_switched_model_interface/include/ocs2_switched_model_interface/constraint/EndEffectorPositionConstraint.h
@@ -1,9 +1,8 @@
-
-
 #pragma once
 
 #include <ocs2_core/automatic_differentiation/CppAdInterface.h>
 #include <ocs2_switched_model_interface/constraint/ConstraintTerm.h>
+#include <ocs2_switched_model_interface/constraint/EndEffectorConstraint.h>
 
 #include "ocs2_switched_model_interface/core/ComModelBase.h"
 #include "ocs2_switched_model_interface/core/KinematicsModelBase.h"
@@ -13,18 +12,20 @@
 
 namespace switched_model {
 
-struct EndEffectorPositionConstraintSettings {
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  // Desired end effector position specified as h(x,u) = A*p(x,u) + b
-  Eigen::MatrixXd A;
-  Eigen::VectorXd b;
-};
+struct EndEffectorPositionConstraintSettings : switched_model::EndEffectorConstraintSettings {};
 
-class EndEffectorPositionConstraint final : public ocs2::ConstraintTerm<STATE_DIM, INPUT_DIM> {
+class EndEffectorPositionConstraint : public switched_model::EndEffectorConstraint {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  using BASE = ocs2::ConstraintTerm<STATE_DIM, INPUT_DIM>;
+  using BASE = switched_model::EndEffectorConstraint;
+  using typename BASE::ad_com_model_t;
+  using typename BASE::ad_dynamic_vector_t;
+  using typename BASE::ad_interface_t;
+  using typename BASE::ad_kinematic_model_t;
+  using typename BASE::ad_scalar_t;
+  using typename BASE::constraint_timeStateInput_matrix_t;
+  using typename BASE::dynamic_vector_t;
   using typename BASE::input_matrix_t;
   using typename BASE::input_state_matrix_t;
   using typename BASE::input_vector_t;
@@ -34,52 +35,17 @@ class EndEffectorPositionConstraint final : public ocs2::ConstraintTerm<STATE_DI
   using typename BASE::scalar_t;
   using typename BASE::state_matrix_t;
   using typename BASE::state_vector_t;
-
- private:
-  static constexpr size_t domain_dim_ = 1 + STATE_DIM + INPUT_DIM;
-  static constexpr size_t range_dim_ = 3;
-
-  using ad_interface_t = ocs2::CppAdInterface<scalar_t>;
-  using ad_scalar_t = typename ad_interface_t::ad_scalar_t;
-  using ad_dynamic_vector_t = typename ad_interface_t::ad_dynamic_vector_t;
-  using dynamic_vector_t = typename ad_interface_t::dynamic_vector_t;
-
-  using constraint_timeStateInput_matrix_t = Eigen::Matrix<scalar_t, -1, 1 + STATE_DIM + INPUT_DIM>;
-  using timeStateInput_matrix_t = Eigen::Matrix<scalar_t, 1 + STATE_DIM + INPUT_DIM, 1 + STATE_DIM + INPUT_DIM>;
-
- public:
-  using ad_com_model_t = ComModelBase<ad_scalar_t>;
-  using ad_kinematic_model_t = KinematicsModelBase<ad_scalar_t>;
+  using typename BASE::timeStateInput_matrix_t;
 
   explicit EndEffectorPositionConstraint(int legNumber, EndEffectorPositionConstraintSettings settings, ad_com_model_t& adComModel,
-                                         ad_kinematic_model_t& adKinematicsModel, bool generateModels)
-      : BASE(ocs2::ConstraintOrder::Quadratic),
-        legNumber_(legNumber),
-        settings_(std::move(settings)),
-        libName_("EEPositionConstraint_" + std::to_string(legNumber_)),
-        libFolder_("/tmp/ocs2") {
-    setAdInterface(adComModel, adKinematicsModel);
-    setAdInterface(adComModel, adKinematicsModel);
-    if (generateModels) {
-      adInterface_->createModels(ad_interface_t::ApproximationOrder::Second, true);
-    } else {
-      adInterface_->loadModelsIfAvailable(ad_interface_t::ApproximationOrder::Second, true);
-    }
-  }
+                                         ad_kinematic_model_t& adKinematicsModel, bool generateModels,
+                                         std::string constraintPrefix = "EEPositionConstraint_")
+      : BASE(ocs2::ConstraintOrder::Quadratic, constraintPrefix, legNumber, std::move(settings), adComModel, adKinematicsModel,
+             generateModels) {}
 
-  EndEffectorPositionConstraint(const EndEffectorPositionConstraint& rhs)
-      : BASE(rhs),
-        legNumber_(rhs.legNumber_),
-        settings_(rhs.settings_),
-        libName_(rhs.libName_),
-        libFolder_(rhs.libFolder_),
-        adInterface_(new ad_interface_t(*rhs.adInterface_)) {}
+  EndEffectorPositionConstraint(const EndEffectorPositionConstraint& rhs) : BASE(rhs) {}
 
   EndEffectorPositionConstraint* clone() const override { return new EndEffectorPositionConstraint(*this); }
-
-  void configure(const EndEffectorPositionConstraintSettings& settings) { settings_ = settings; };
-
-  size_t getNumConstraints(scalar_t time) const override { return settings_.A.rows(); };
 
   scalar_array_t getValue(scalar_t time, const state_vector_t& state, const input_vector_t& input) const override {
     // Assemble input
@@ -96,50 +62,6 @@ class EndEffectorPositionConstraint final : public ocs2::ConstraintTerm<STATE_DI
     }
     return constraintValue;
   };
-
-  LinearApproximation_t getLinearApproximation(scalar_t time, const state_vector_t& state, const input_vector_t& input) const override {
-    // Assemble input
-    dynamic_vector_t tapedInput(1 + STATE_DIM + INPUT_DIM);
-    tapedInput << time, state, input;
-
-    // Compute end effector velocity and derivatives
-    constraint_timeStateInput_matrix_t stateInputJacobian = adInterface_->getJacobian(tapedInput);
-
-    // Collect constraint terms
-    auto dhdx = stateInputJacobian.template middleCols<STATE_DIM>(1);
-    auto dhdu = stateInputJacobian.template rightCols<INPUT_DIM>();
-
-    // Convert to output format
-    LinearApproximation_t linearApproximation;
-    linearApproximation.constraintValues = getValue(time, state, input);
-    for (int i = 0; i < settings_.A.rows(); i++) {
-      linearApproximation.derivativeState.emplace_back(settings_.A.row(i) * dhdx);
-      linearApproximation.derivativeInput.emplace_back(settings_.A.row(i) * dhdu);
-    }
-    return linearApproximation;
-  }
-
-  QuadraticApproximation_t getQuadraticApproximation(scalar_t time, const state_vector_t& state,
-                                                     const input_vector_t& input) const override {
-    // Assemble input
-    dynamic_vector_t tapedInput(1 + STATE_DIM + INPUT_DIM);
-    tapedInput << time, state, input;
-
-    // Convert to output format
-    QuadraticApproximation_t quadraticApproximation;
-    auto linearApproximation = getLinearApproximation(time, state, input);
-    quadraticApproximation.constraintValues = std::move(linearApproximation.constraintValues);
-    quadraticApproximation.derivativeState = std::move(linearApproximation.derivativeState);
-    quadraticApproximation.derivativeInput = std::move(linearApproximation.derivativeInput);
-    for (int i = 0; i < settings_.A.rows(); i++) {
-      timeStateInput_matrix_t weightedHessian = adInterface_->getHessian(settings_.A.row(i), tapedInput);
-
-      quadraticApproximation.secondDerivativesState.emplace_back(weightedHessian.block(1, 1, STATE_DIM, STATE_DIM));
-      quadraticApproximation.secondDerivativesInput.emplace_back(weightedHessian.block(1 + STATE_DIM, 1 + STATE_DIM, INPUT_DIM, INPUT_DIM));
-      quadraticApproximation.derivativesInputState.emplace_back(weightedHessian.block(1 + STATE_DIM, 1, INPUT_DIM, STATE_DIM));
-    }
-    return quadraticApproximation;
-  }
 
  private:
   void adFootPosition(ad_com_model_t& adComModel, ad_kinematic_model_t& adKinematicsModel, const ad_dynamic_vector_t& tapedInput,
@@ -159,18 +81,13 @@ class EndEffectorPositionConstraint final : public ocs2::ConstraintTerm<STATE_DI
     o_footPosition = adKinematicsModel.footPositionInOriginFrame(legNumber_, basePose, qJoints);
   }
 
-  void setAdInterface(ad_com_model_t& adComModel, ad_kinematic_model_t& adKinematicsModel) {
+  void setAdInterface(ad_com_model_t& adComModel, ad_kinematic_model_t& adKinematicsModel) override {
+    // Function to differentiate
     auto adfunc = [this, &adComModel, &adKinematicsModel](const ad_dynamic_vector_t& x, ad_dynamic_vector_t& y) {
       this->adFootPosition(adComModel, adKinematicsModel, x, y);
     };
 
-    adInterface_.reset(new ad_interface_t(adfunc, range_dim_, domain_dim_, libName_, libFolder_));
+    adInterface_.reset(new ad_interface_t(adfunc, BASE::range_dim_, BASE::domain_dim_, libName_, libFolder_));
   };
-
-  int legNumber_;
-  EndEffectorPositionConstraintSettings settings_;
-  std::string libName_;
-  std::string libFolder_;
-  std::unique_ptr<ad_interface_t> adInterface_;
 };
 }  // namespace switched_model

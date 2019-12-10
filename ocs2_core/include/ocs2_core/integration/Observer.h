c560af9d06b2c2ae8d4f5f224c07efbbd7ceb55c
Farbod Farshidian <farbod.farshidian@gmail.com>
2019-12-10 18:01:02 +0100

    update to Observer

--- ocs2_core/include/ocs2_core/integration/Observer.h
+++ ocs2_core/include/ocs2_core/integration/Observer.h
@@ -60,48 +60,57 @@ class Observer {
   using model_data_array_t = model_data_t::array_t;
 
   /**
-   * Constructor
-   * @param [out] stateTraj: State trajectory
-   * @param [out] timeTraj: Time of each trajectory point
-   * @param [out] modelDataTraj: Model data trajectory
+   * Constructor.
+   *
+   * @param stateTrajectoryPtr: A pinter to an state trajectory container to store resulting state trajectory.
+   * @param timeTrajectoryPtr: A pinter to an time trajectory container to store resulting time trajectory.
+   * @param modelDataTrajectoryPtr: A pinter to an model data trajectory container to store resulting model data trajectory.
    */
-  explicit Observer(state_vector_array_t* stateTraj = nullptr, scalar_array_t* timeTraj = nullptr,
-                    model_data_array_t* modelDataTraj = nullptr)
-      : timeTrajectory_(timeTraj), stateTrajectory_(stateTraj), modelDataTrajectory_(modelDataTraj), initialCall_(false) {}
+  explicit Observer(state_vector_array_t* stateTrajectoryPtr = nullptr, scalar_array_t* timeTrajectoryPtr = nullptr,
+                    model_data_array_t* modelDataTrajectoryPtr = nullptr)
+      : initialCall_(false),
+        timeTrajectoryPtr_(timeTrajectoryPtr),
+        stateTrajectoryPtr_(stateTrajectoryPtr),
+        modelDataTrajectoryPtr_(modelDataTrajectoryPtr) {}
+
+  /**
+   * Default destructor.
+   */
+  ~Observer() = default;
 
   /**
    * Observe function to retrieve the variable of interest.
    * @param [in] system: system dynamics object.
-   * @param [in] t: Current time.
-   * @param [in] x: Current state.
+   * @param [in] state: Current state.
+   * @param [in] time: Current time.
    */
-  void observe(OdeBase<STATE_DIM>& system, const state_vector_t& x, const scalar_t& t) {
+  void observe(OdeBase<STATE_DIM>& system, const state_vector_t& state, const scalar_t& time) {
     // Store data
-    if (stateTrajectory_) {
-      stateTrajectory_->push_back(x);
+    if (stateTrajectoryPtr_) {
+      stateTrajectoryPtr_->push_back(state);
     }
-    if (timeTrajectory_) {
-      timeTrajectory_->push_back(t);
+    if (timeTrajectoryPtr_) {
+      timeTrajectoryPtr_->push_back(time);
     }
 
     // extract model data
-    if (modelDataTrajectory_) {
+    if (modelDataTrajectoryPtr_) {
       // check for initial call
       if (system.nextModelDataPtrIterator() == system.beginModelDataPtrIterator()) {
         // do nothing, model data not yet available.
         initialCall_ = true;
       } else if (initialCall_) {
-        // this is second observer call, retreive model data from initial call.
+        // this is second observer call, retrieve model data from initial call.
         model_data_t* modelDataPtr = system.beginModelDataPtrIterator()->get();
-        modelDataTrajectory_->emplace_back(*modelDataPtr);
+        modelDataTrajectoryPtr_->emplace_back(*modelDataPtr);
         initialCall_ = false;
       }
-      // get model data from current timestep
+      // get model data from current time step
       while (system.nextModelDataPtrIterator() != system.beginModelDataPtrIterator()) {
         --system.nextModelDataPtrIterator();
         model_data_t* modelDataPtr = system.nextModelDataPtrIterator()->get();
-        if (numerics::almost_eq(modelDataPtr->time_, t)) {
-          modelDataTrajectory_->emplace_back(*modelDataPtr);
+        if (numerics::almost_eq(modelDataPtr->time_, time)) {
+          modelDataTrajectoryPtr_->emplace_back(*modelDataPtr);
           break;
         }
       }
@@ -112,10 +121,9 @@ class Observer {
 
  private:
   bool initialCall_;
-
-  state_vector_array_t* stateTrajectory_;
-  scalar_array_t* timeTrajectory_;
-  model_data_array_t* modelDataTrajectory_;
+  scalar_array_t* timeTrajectoryPtr_;
+  state_vector_array_t* stateTrajectoryPtr_;
+  model_data_array_t* modelDataTrajectoryPtr_;
 };
 
 }  // namespace ocs2

a4462f7a69aba54389ebbb81a412bebb07c7961c
Koen de Vos <k.d.vos@student.tue.nl>
2019-12-13 14:53:06 +0100

    Comments on bouncing mass

--- ocs2_ddp/test/bouncingmass/include/OverallReference.h
+++ ocs2_ddp/test/bouncingmass/include/OverallReference.h
@@ -1,28 +1,100 @@
 #pragma once
 
 #include "Reference.h"
-
+/*
+ * 	This class constructs, contains and provides the reference for the
+ * 	BouncingMass SLQ test. The reference is based on the reference presented
+ * 	in [On Optimal Trajectory Tracking for Mechanical Systems with Unilateral Constraints
+ * by M. Rijnen]
+ */
 class OverallReference
 {
 	using dynamic_vector_t = Eigen::Matrix<double, Eigen::Dynamic, 1>;
 	using dynamic_vector_array_t = std::vector<dynamic_vector_t, Eigen::aligned_allocator<dynamic_vector_t>>;
 
-	public:
+public:
+	/*
+	 * Constructor
+	 */
 	OverallReference() = default;
-	OverallReference(std::vector<double> traj_times, std::vector<Eigen::Vector3d> traj_state);
 
+	/*
+	 * Constructor
+	 *
+	 * @param [in] trajTimes: list of times at which the reference is defined
+	 * @param [in] trajState: list of waypoints at which the reference is defined
+	 */
+	OverallReference(std::vector<double> trajTimes, std::vector<Eigen::Vector3d> trajState);
+
+	/*
+	 * Calculate the input at a certain time
+	 *
+	 * @param [in] time: time moment at which the input is calculated
+	 * @param [out] input: input corresponding to time
+	 */
 	void getInput(double time, double &input);
+
+	/*
+	 * Calculate the input at a range of times
+	 *
+	 * @param [in] t0: First time element of output
+	 * @param [in] t1: Last time element of output
+	 * @param [in] dt: Time step of output vectors
+	 * @param [out] time: Time vector
+	 * @param [out] input: Input vector corresponding to time vector
+	 */
 	void getInput(const double t0, const double t1, double dt, std::vector<double>& time, std::vector<double>& input);
 
+	/*
+	 * Calculate the reference state at a certain time
+	 *
+	 * @param [in] time: time moment at which the input is calculated
+	 * @param [out] state: state corresponding to time
+	 */
 	void getState(double time, Eigen::Vector3d &x);
+
+	/*
+	 * Calculate the reference state at a certain time and mode
+	 *
+	 * @param [in] idx: mode at which the input is calculated
+	 * @param [in] time: time moment at which the input is calculated
+	 * @param [out] state: state corresponding to time and mode
+	 */
 	void getState(int idx, double time, Eigen::Vector3d &x);
 
+	/*
+	 * Extend the reference past the event times, by integrating the input signal
+	 * without applying the jump map
+	 *
+	 * @param [in] delta: length of the extensions
+	 */
 	void extendref(double delta);
 
+	/*
+	 * Display the reference
+	 *
+	 * @param [in] i: choice of which section of the reference is displayed
+	 */
 	void display(int i);
 
-	private:
+private:
+
+	/*
+	 * Find the index of the currently active reference
+	 *
+	 * @param [in] time: time moment at which the index is requested
+	 *
+	 * @return currently active index
+	 */
 	int getIndex(double time);
+
+	/*
+	 * Jump map of the system
+	 *
+	 * @param [in] x: State before event
+	 *
+	 * @return currently active index
+	 */
 	void jumpMap(Eigen::Vector3d &x);
 
 	std::vector<Reference> 	References_;
--- ocs2_ddp/test/bouncingmass/include/Reference.h
+++ ocs2_ddp/test/bouncingmass/include/Reference.h
@@ -9,31 +9,95 @@
 
 #include "ReferenceModel.h"
 #include "Observer.h"
-
+/*
+ * 	This class constructs, contains a section of the reference, which is contained in
+ * 	OverallReference and used in BouncingMass SLQ test.
+ * 	The reference is based on the reference presented
+ * 	in [On Optimal Trajectory Tracking for Mechanical Systems with Unilateral Constraints
+ * by M. Rijnen]
+ */
 class Reference
 {
 
 public:
 
+	/*
+	 * Constructor
+	 */
 	Reference() = default;
+
+	/*
+	 * Constructor
+	 *
+	 * @param [in] t0: Time at which the first waypoint of the reference is defined
+	 * @param [in] t1: Time at which the second waypoint of the reference is defined
+	 * @param [in] p0: First waypoint of the reference [position,velocity,acceleration]
+	 * @param [in] p1: Second waypoint of the reference
+	 */
 	Reference(double t0, double t1, Eigen::Vector3d p0, Eigen::Vector3d p1);
 
-	void getInput(double time,double &input);
-	void getState(double time,Eigen::Vector3d &x);
+	/*
+	 * Obtain reference input at the current time
+	 *
+	 * @param [in] time: current time
+	 * @param [out] input: current reference input
+	 */
+	void getInput(const double time,double &input);
 
-	void extendref(double delta, Reference* ref_pre, Reference* ref_post);
+	/*
+	 * Obtain reference state at current time
+	 *
+	 * @param [in] time: current time
+	 * @param [out] x: current reference state
+	 */
+	void getState(const double time,Eigen::Vector3d &x);
 
+	/*
+	 * Extend the reference by integrating the input signal of the next
+	 * and previous sections
+	 *
+	 * @param [in] delta: extension time
+	 * @param [in] refPre: pointer to previous section of the reference
+	 * @param [in] refPost: pointer to the next section of the reference
+	 */
+	void extendref(double delta, Reference* refPre, Reference* refPost);
+
+	/*
+	 * Display the reference
+	 */
 	void display();
 
 	double t0_;
 	double t1_;
 	double delta_ = 0;
-
-
 private:
 
+	/*
+	 * 	Find the polynomial coefficients corresponding to the posed constraints
+	 * 	on position, velocity and accleration at the posed times
+	 *
+	 * 	@param [in]	t0:	Time moment of first waypoint
+	 * 	@param [in] t1: time moment of second waypoint
+	 * 	@param [in] p0: first waypoint
+	 * 	@param [in] p1: second waypoint
+	 */
 	void Create5thOrdPol(double t0, double t1, Eigen::Vector3d p0, Eigen::Vector3d p1);
+
+	/*
+	 * 	Find the derivative of the polynomial described by a
+	 * 	set of coefficients
+	 *
+	 * 	@param [in]	pol: coefficients describing the fifth order polynomial
+	 */
 	Eigen::Matrix<double, 6 , 1> polyder(Eigen::Matrix<double, 6 , 1> pol);
+
+	/*
+	 * 	Interpolate the extended reference signals to find the reference
+	 * 	corresponding to the current time
+	 *
+	 * 	@param [in]	time:	current time
+	 * 	@param [out] x: current state
+	 */
 	void interpolate_ext(double time, Eigen::Vector3d &x);
 
 	Eigen::Matrix<double, 6, 1> polU_;

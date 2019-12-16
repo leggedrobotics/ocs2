01f93b6b0a3e365f6aa208d586e3dd7fc26beb95
Koen de Vos <k.d.vos@student.tue.nl>
2019-12-16 15:56:06 +0100

    Slightly alter event detection in controller

--- ocs2_core/include/ocs2_core/control/LinearController.h
+++ ocs2_core/include/ocs2_core/control/LinearController.h
@@ -275,19 +275,19 @@ class LinearController final : public ControllerBase<STATE_DIM, INPUT_DIM> {
       return scalar_array_t(0);
     }
 
-    scalar_array_t eventTimes;
+    scalar_array_t eventTimes(0);
     scalar_t lastevent = timeStamp_[0];
 
     for (int i = 0; i < timeStamp_.size() - 1; i++) {
-   	  bool eventDetected = timeStamp_[i + 1] - timeStamp_[i] <= 2 * OCS2NumericTraits<scalar_t>::weakEpsilon();
-   	  bool sufficientTimeSinceEvent = timeStamp_[i] - lastevent > 10 * OCS2NumericTraits<scalar_t>::weakEpsilon();
+   	  bool eventDetected = timeStamp_[i + 1] - timeStamp_[i] < 2.0 * OCS2NumericTraits<scalar_t>::weakEpsilon();
+   	  bool sufficientTimeSinceEvent = timeStamp_[i] - lastevent > 2.0 * OCS2NumericTraits<scalar_t>::weakEpsilon();
 
       if (eventDetected && sufficientTimeSinceEvent)
-      {		// Push back event when event is detected
+      {	  // push back event when event is detected
         eventTimes.push_back(timeStamp_[i]);
         lastevent = eventTimes.back();
       } else if (eventDetected) {
-    	  // If event is detected to close to the last event, it is assumed that the earlier event was not an event
+    	  // if event is detected to close to the last event, it is assumed that the earlier event was not an event
     	  // but was due to the refining steps taken in event detection
     	  // The last "detected event" is the time the event took place
         eventTimes.back() = timeStamp_[i];

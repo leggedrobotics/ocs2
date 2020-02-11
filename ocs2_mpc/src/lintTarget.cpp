8b35e0942e524e34dcf93a451104438b34744c1d
Farbod Farshidian <farbod.farshidian@gmail.com>
2020-02-11 14:55:37 +0100

    ocs2_ocs2 is temporarily disabled from ocs2_mpc

--- ocs2_mpc/src/lintTarget.cpp
+++ ocs2_mpc/src/lintTarget.cpp
@@ -1,6 +1,6 @@
 #include <ocs2_mpc/MPC_BASE.h>
 #include <ocs2_mpc/MPC_ILQR.h>
-#include <ocs2_mpc/MPC_OCS2.h>
+//#include <ocs2_mpc/MPC_OCS2.h>
 #include <ocs2_mpc/MPC_SLQ.h>
 #include <ocs2_mpc/MPC_Settings.h>
 

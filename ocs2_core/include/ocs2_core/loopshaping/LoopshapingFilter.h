6dbb58258a51622012edd9a637263bda3fda34d1
rgrandia <rubengrandia@gmail.com>
2020-02-14 14:00:25 +0100

    introduce cpp files for loopshaping where possible

--- ocs2_core/include/ocs2_core/loopshaping/LoopshapingFilter.h
+++ ocs2_core/include/ocs2_core/loopshaping/LoopshapingFilter.h
@@ -1,7 +1,6 @@
 
 
-#ifndef OCS2_LOOPSHAPINGFILTER_H
-#define OCS2_LOOPSHAPINGFILTER_H
+#pragma once
 
 #include <ocs2_core/dynamics/TransferFunctionBase.h>
 #include <Eigen/Dense>
@@ -13,17 +12,9 @@ class Filter {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  Filter() {
-    A_.resize(0, 0);
-    B_.resize(0, 0);
-    C_.resize(0, 0);
-    D_.resize(0, 0);
-  };
+  Filter();
 
-  Filter(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& C, const Eigen::MatrixXd& D)
-      : A_(A), B_(B), C_(C), D_(D), numStates_(A.rows()), numInputs_(B.cols()), numOutputs_(C.rows()) {
-    checkSize();
-  }
+  Filter(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& C, const Eigen::MatrixXd& D);
 
   size_t getNumStates() const { return numStates_; };
   size_t getNumInputs() const { return numInputs_; };
@@ -34,15 +25,7 @@ class Filter {
   const Eigen::MatrixXd& getC() const { return C_; };
   const Eigen::MatrixXd& getD() const { return D_; };
 
-  void print() const {
-    std::cout << "numStates: " << numStates_ << std::endl;
-    std::cout << "numInputs: " << numInputs_ << std::endl;
-    std::cout << "numOutputs: " << numOutputs_ << std::endl;
-    std::cout << "A: \n" << A_ << std::endl;
-    std::cout << "B: \n" << B_ << std::endl;
-    std::cout << "C: \n" << C_ << std::endl;
-    std::cout << "D: \n" << D_ << std::endl;
-  }
+  void print() const;
 
   template <typename DerivedOutput, typename DerivedState, typename DerivedInput>
   void findEquilibriumForOutput(const DerivedOutput& y, DerivedState& x, DerivedInput& u) const {
@@ -72,23 +55,7 @@ class Filter {
   }
 
  private:
-  void checkSize() const {
-    bool correct = true;
-    // check number of state
-    correct &= numStates_ == A_.rows();
-    correct &= numStates_ == B_.rows();
-    correct &= numStates_ == C_.cols();
-    // Check number of inputs
-    correct &= numInputs_ == B_.cols();
-    correct &= numInputs_ == D_.cols();
-    // Check number of outputs
-    correct &= numOutputs_ == C_.rows();
-    correct &= numOutputs_ == D_.rows();
-    if (!correct) {
-      print();
-      throw std::runtime_error("Loopshaping: Filer: Matrix dimensions not consistent.");
-    }
-  }
+  void checkSize() const;
 
   Eigen::MatrixXd A_, B_, C_, D_;
   size_t numStates_ = 0;
@@ -97,5 +64,3 @@ class Filter {
 };
 
 }  // namespace ocs2
-
-#endif  // OCS2_LOOPSHAPINGFILTER_H

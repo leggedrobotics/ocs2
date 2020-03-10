e0ee26a78a48df7a9282b1019c3d46015dd725b9
Marko Bjelonic <marko-bjelonic@t-online.de>
2020-03-10 18:21:31 +0100

    Added ocs2_anymal_wheels_loopshaping/

---
+++ anymal_wheels/ocs2_anymal_wheels_loopshaping/src/AnymalWheelsLoopshapingDummyMrt.cpp
@@ -0,0 +1,50 @@
+//
+// Created by rgrandia on 13.02.20.
+//
+
+#include <ocs2_mpc/MPC_Settings.h>
+#include <ocs2_quadruped_loopshaping_interface/QuadrupedLoopshapingDummyNode.h>
+
+#include "ocs2_anymal_wheels_loopshaping/AnymalWheelsLoopshapingInterface.h"
+
+// URDF stuff
+#include <robot_state_publisher/robot_state_publisher.h>
+#include <urdf/model.h>
+#include <future>
+#include <kdl_parser/kdl_parser.hpp>
+
+int main(int argc, char* argv[]) {
+  if (argc <= 1) {
+    throw std::runtime_error("No task file specified. Aborting.");
+  }
+  const std::string taskName(argv[1]);  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic))
+
+  // Initialize ros node
+  ros::init(argc, argv, "anymal_wheels_loopshaping_mrt");
+  ros::NodeHandle nodeHandle;
+
+  // Publish wheel angles in separate thread
+  // Load URDF model
+  urdf::Model urdfModel;
+  if (!urdfModel.initParam("ocs2_anymal_description")) {
+    throw std::runtime_error("[AnymalWheelsDummyMrt] Could not read URDF from: \"ocs2_anymal_description\"");
+  }
+  KDL::Tree kdlTree;
+  kdl_parser::treeFromUrdfModel(urdfModel, kdlTree);
+  robot_state_publisher::RobotStatePublisher robotStatePublisher(kdlTree);
+  auto wheelPublisher = std::async(std::launch::async, [&]() {
+    ros::WallRate rate(50);
+    while (ros::ok() && ros::master::check()) {
+      robotStatePublisher.publishTransforms({{"LF_WHEEL", 0.0}, {"RF_WHEEL", 0.0}, {"LH_WHEEL", 0.0}, {"RH_WHEEL", 0.0}}, ros::Time::now(),
+                                            "");
+      rate.sleep();
+    }
+  });
+
+  auto anymalInterface = anymal::getAnymalWheelsLoopshapingInterface(taskName);
+  ocs2::MPC_Settings mpcSettings;
+  mpcSettings.loadSettings(anymal::getTaskFilePathAnymalWheelsLoopshaping(taskName));
+  quadrupedLoopshapingDummyNode(nodeHandle, *anymalInterface, mpcSettings.mrtDesiredFrequency_, mpcSettings.mpcDesiredFrequency_);
+
+  return 0;
+}
---
+++ anymal_wheels/ocs2_anymal_wheels_loopshaping/src/AnymalWheelsLoopshapingInterface.cpp
@@ -0,0 +1,39 @@
+//
+// Created by rgrandia on 13.02.20.
+//
+
+#include "ocs2_anymal_wheels_loopshaping/AnymalWheelsLoopshapingInterface.h"
+
+#include <ros/package.h>
+
+#include <ocs2_anymal_wheels_switched_model/core/AnymalWheelsCom.h>
+#include <ocs2_anymal_wheels_switched_model/core/AnymalWheelsKinematics.h>
+
+namespace anymal {
+
+std::unique_ptr<switched_model_loopshaping::QuadrupedLoopshapingInterface> getAnymalWheelsLoopshapingInterface(const std::string& taskName) {
+  std::string taskFolder = getTaskFileFolderAnymalWheelsLoopshaping(taskName);
+  std::cerr << "Loading task file from: " << taskFolder << std::endl;
+
+  auto kin = AnymalWheelsKinematics();
+  auto kinAd = AnymalWheelsKinematicsAd();
+  auto com = AnymalWheelsCom();
+  auto comAd = AnymalWheelsComAd();
+
+  auto quadrupedInterface =
+      std::unique_ptr<switched_model::QuadrupedInterface>(new switched_model::QuadrupedInterface(kin, kinAd, com, comAd, taskFolder));
+
+  return std::unique_ptr<switched_model_loopshaping::QuadrupedLoopshapingInterface>(
+      new switched_model_loopshaping::QuadrupedLoopshapingInterface(std::move(quadrupedInterface), taskFolder));
+}
+
+std::string getTaskFileFolderAnymalWheelsLoopshaping(const std::string& taskName) {
+  std::string taskFolder = ros::package::getPath("ocs2_anymal_wheels_loopshaping") + "/config/" + taskName;
+  return taskFolder;
+}
+
+std::string getTaskFilePathAnymalWheelsLoopshaping(const std::string& taskName) {
+  return getTaskFileFolderAnymalWheelsLoopshaping(taskName) + "/task.info";
+}
+
+}  // end of namespace anymal

bc933b231c129541f532ce4fc4310d44d9a3d5ef
Oliver Harley <oharley@student.ethz.ch>
2020-05-07 02:05:24 +0200

    take targetCommand filepath as program argument

--- common/ocs2_anymal_commands/src/AnymalTargetVelocityCommand.cpp
+++ common/ocs2_anymal_commands/src/AnymalTargetVelocityCommand.cpp
@@ -6,16 +6,44 @@
  */
 
 #include <ros/package.h>
+#include <boost/program_options.hpp>
 
 #include <ocs2_core/misc/LoadData.h>
 
 #include "ocs2_anymal_commands/TargetTrajectories_Keyboard_Quadruped.h"
 
 int main(int argc, char* argv[]) {
-  std::string filename = ros::package::getPath("ocs2_anymal_commands") + "/config/targetCommand.info";
+  std::string filename;
+  {
+    namespace po = boost::program_options;
+    po::options_description desc{"Options"};
+    // clang-format off
+    desc.add_options()
+      ("help,h", "Help")
+      ("targetcommand,t",
+       po::value<std::string>(&filename)->default_value(
+         ros::package::getPath("ocs2_anymal_commands") + "/config/targetCommand.info"),
+         "TargetCommands file path"
+         );
+    // clang-format on
+
+    po::variables_map vm;
+    po::store(parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << '\n';
+      return 1;  // Fail, and exit.
+    }
+  }
 
   boost::property_tree::ptree pt;
-  boost::property_tree::read_info(filename, pt);
+  try {
+    boost::property_tree::read_info(filename, pt);
+  } catch (boost::property_tree::ptree_bad_path) {
+    std::cout << "Could not find/read " << filename << "." << std::endl;
+    return 2;  // Fail, and exit.
+  }
 
   const auto targetDisplacementVelocity = pt.get<double>("targetDisplacementVelocity");
   const auto targetRotationVelocity = pt.get<double>("targetRotationVelocity");

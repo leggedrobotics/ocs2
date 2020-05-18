52279b7ebf6873bae5eb4e0ecf5a6c062da9577c
rgrandia <rubengrandia@gmail.com>
2020-05-18 14:46:39 +0200

    fix setGaitAt when insertion time is within current gait cycle

--- common/ocs2_switched_model_interface/src/logic/GaitSchedule.cpp
+++ common/ocs2_switched_model_interface/src/logic/GaitSchedule.cpp
@@ -48,7 +48,12 @@ void GaitSchedule::setGaitSequenceAtTime(const std::vector<Gait>& gaitSequence, 
   std::tie(newPhase, newActiveGait) = advancePhase(phase_, time - time_, gaitSchedule_.begin(), gaitSchedule_.end());
 
   // Shrink the gait that is active at "time", s.t. it ends at "time"
-  newActiveGait->duration *= newPhase;
+  if (newActiveGait == gaitSchedule_.begin()) {
+    // Gait to shrink is already active. Determine new duration from the amount of time and phase left.
+    newActiveGait->duration = (time - time_) / (1.0 - phase_);
+  } else {
+    newActiveGait->duration *= newPhase;
+  }
 
   gaitSchedule_.erase(newActiveGait + 1, gaitSchedule_.end());
   gaitSchedule_.insert(gaitSchedule_.end(), gaitSequence.begin(), gaitSequence.end());

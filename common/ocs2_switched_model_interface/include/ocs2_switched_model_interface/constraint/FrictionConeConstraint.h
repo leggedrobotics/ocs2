f800f84665a520a7ff9495648e90f8a9e4537c7a
rgrandia <rubengrandia@gmail.com>
2020-08-12 14:06:05 +0200

    add documentation

--- common/ocs2_switched_model_interface/include/ocs2_switched_model_interface/constraint/FrictionConeConstraint.h
+++ common/ocs2_switched_model_interface/include/ocs2_switched_model_interface/constraint/FrictionConeConstraint.h
@@ -8,7 +8,15 @@ namespace switched_model {
 /**
  * Implements the constraint h(t,x,u) >= 0
  *
- * frictionCoefficient_ * Fz - sqrt(Fx * Fx + Fy * Fy + regularization_) >= 0
+ * frictionCoefficient * (Fz + gripperForce) - sqrt(Fx * Fx + Fy * Fy + regularization) >= 0
+ *
+ * The gripper force shifts the origin of the friction cone down in z-direction by the amount of gripping force available. This makes it
+ * possible to produce tangential forces without applying a regular normal force on that foot, or to "pull" on the foot with magnitude up to
+ * the gripping force.
+ *
+ * The regularization prevents the constraint gradient / hessian to go to infinity when Fx = Fz = 0. It also creates a parabolic safety
+ * margin to the friction cone. For example: when Fx = Fy = 0 the constraint zero-crossing will be at Fz = 1/frictionCoefficient *
+ * sqrt(regularization) instead of Fz = 0
  *
  */
 class FrictionConeConstraint final : public ocs2::ConstraintTerm<STATE_DIM, INPUT_DIM> {
@@ -24,6 +32,13 @@ class FrictionConeConstraint final : public ocs2::ConstraintTerm<STATE_DIM, INPU
   using typename BASE::state_matrix_t;
   using typename BASE::state_vector_t;
 
+  /**
+   * Constructor
+   * @param frictionCoefficient : Friction coefficient (>0).
+   * @param regularization : (>0), see class documentation.
+   * @param legNumber : leg index in {0, 1, 2, 3}.
+   * @param gripperForce : Gripper force in normal direction.
+   */
   FrictionConeConstraint(scalar_t frictionCoefficient, scalar_t regularization, int legNumber, scalar_t gripperForce = 0.0);
 
   ~FrictionConeConstraint() override = default;
@@ -42,8 +57,6 @@ class FrictionConeConstraint final : public ocs2::ConstraintTerm<STATE_DIM, INPU
                                                      const input_vector_t& input) const override;
 
  private:
-  using LocalForces = vector3_t;
-
   struct LocalForceDerivatives {
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     matrix3_t dF_deuler;  // derivative local force w.r.t. body euler angles
@@ -65,10 +78,10 @@ class FrictionConeConstraint final : public ocs2::ConstraintTerm<STATE_DIM, INPU
     matrix3_t d2Cone_deuler2;
   };
 
-  scalar_t coneConstraint(const LocalForces& localForces) const;
-  LocalForces computeLocalForces(const vector3_t& eulerXYZ, const vector3_t& forcesInBodyFrame) const;
+  scalar_t coneConstraint(const vector3_t& localForces) const;
+  vector3_t computeLocalForces(const vector3_t& eulerXYZ, const vector3_t& forcesInBodyFrame) const;
   LocalForceDerivatives computeLocalForceDerivatives(const vector3_t& eulerXYZ, const vector3_t& forcesInBodyFrame) const;
-  ConeLocalDerivatives computeConeLocalDerivatives(const LocalForces& localForces) const;
+  ConeLocalDerivatives computeConeLocalDerivatives(const vector3_t& localForces) const;
   ConeDerivatives computeConeConstraintDerivatives(const ConeLocalDerivatives& coneLocalDerivatives,
                                                    const LocalForceDerivatives& localForceDerivatives) const;
 

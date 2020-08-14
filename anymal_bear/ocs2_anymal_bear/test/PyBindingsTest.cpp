b745d06af7f2fbec513387906a8eaf585943d66d
Michael Spieler <mspieler@ethz.ch>
2020-08-14 11:05:21 +0200

    Fix anymal bear python bindings

--- anymal_bear/ocs2_anymal_bear/test/PyBindingsTest.cpp
+++ anymal_bear/ocs2_anymal_bear/test/PyBindingsTest.cpp
@@ -8,19 +8,10 @@
 #include <ocs2_anymal_bear/AnymalBearPyBindings.h>
 
 TEST(Anymal, PyBindings) {
-  using bindings_t = anymal::AnymalBearPyBindings;
-  using state_vector_t = bindings_t::state_vector_t;
-  using input_vector_t = bindings_t::input_vector_t;
-  using state_matrix_array_t = bindings_t::state_matrix_array_t;
-  using scalar_array_t = bindings_t::scalar_array_t;
-  using state_vector_array_t = bindings_t::state_vector_array_t;
-  using input_vector_array_t = bindings_t::input_vector_array_t;
-  using input_state_matrix_array_t = bindings_t::input_state_matrix_array_t;
-
   const std::string taskFile = "mpc";
-  bindings_t bindings(taskFile, false);
+  anymal::AnymalBearPyBindings bindings(taskFile);
 
-  state_vector_t initState = state_vector_t::Zero();
+  ocs2::vector_t initState = ocs2::vector_t::Zero(switched_model::STATE_DIM);
   initState(5) = 0.495;  // base z
   initState(12) = -0.1;  // LF_HAA
   initState(13) = 0.7;
@@ -34,26 +25,26 @@ TEST(Anymal, PyBindings) {
   initState(21) = 0.1;
   initState(22) = -0.7;
   initState(23) = 1.0;  // RH_KFE
+
+  ocs2::vector_t initInput = ocs2::vector_t::Zero(switched_model::INPUT_DIM);
+  initInput(2) = 79.36;
+  initInput(5) = 77.85;
+  initInput(8) = 81.44;
+  initInput(11) = 79.93;
+
   auto costDesiredTraj = ocs2::CostDesiredTrajectories();
-  costDesiredTraj.desiredTimeTrajectory().resize(1, 0.0);
-
-  costDesiredTraj.desiredInputTrajectory().resize(1, input_vector_t::Zero());
-  costDesiredTraj.desiredInputTrajectory()[0](2) = 79.36;
-  costDesiredTraj.desiredInputTrajectory()[0](5) = 77.85;
-  costDesiredTraj.desiredInputTrajectory()[0](8) = 81.44;
-  costDesiredTraj.desiredInputTrajectory()[0](11) = 79.93;
-
-  costDesiredTraj.desiredStateTrajectory().resize(1, state_vector_t::Zero());
-  costDesiredTraj.desiredStateTrajectory()[0] = initState;
+  costDesiredTraj.desiredTimeTrajectory().push_back(0.0);
+  costDesiredTraj.desiredInputTrajectory().push_back(initInput);
+  costDesiredTraj.desiredStateTrajectory().push_back(initState);
 
   bindings.reset(costDesiredTraj);
   // initState(3) = 0.5; // x position
-  bindings.setObservation(0.0, initState);
+  bindings.setObservation(0.0, initState, initInput);
   bindings.advanceMpc();
 
-  auto t_arr = scalar_array_t();
-  auto x_arr = state_vector_array_t();
-  auto u_arr = input_vector_array_t();
+  ocs2::scalar_array_t t_arr;
+  ocs2::vector_array_t x_arr;
+  ocs2::vector_array_t u_arr;
 
   bindings.getMpcSolution(t_arr, x_arr, u_arr);
 
@@ -66,54 +57,51 @@ TEST(Anymal, PyBindings) {
     std::cout << t_arr[i] << "\t\t" << x_arr[i](3) << "\t" << x_arr[i](14) << std::endl;
   }
 
-  double t = 0.0;
+  ocs2::scalar_t t = 0.0;
   for (int i = 0; i < 301; i++) {
-    bindings.setObservation(t, initState);
+    bindings.setObservation(t, initState, initInput);
     bindings.advanceMpc();
     bindings.getMpcSolution(t_arr, x_arr, u_arr);
     t += 0.01;
   }
 
   std::cout << "computing FlowMap: " << std::endl;
-  auto dxdt = bindings.computeFlowMap(t_arr[0], x_arr[0], u_arr[0]);
+  const auto dxdt = bindings.flowMap(t_arr[0], x_arr[0], u_arr[0]);
   std::cout << "dxdt: " << dxdt.transpose() << std::endl;
 
-  std::cout << "computing FlowMap derivative: " << std::endl;
-  bindings.setFlowMapDerivativeStateAndControl(t_arr[0], x_arr[0], u_arr[0]);
-  auto A = bindings.computeFlowMapDerivativeState();
-  auto B = bindings.computeFlowMapDerivativeInput();
+  const auto flowMap = bindings.flowMapLinearApproximation(t_arr[0], x_arr[0], u_arr[0]);
+  std::cout << "A\n" << flowMap.dfdx << "\nB\n" << flowMap.dfdu << std::endl;
 
-  std::cout << "A\n" << A << "\nB\n" << B << std::endl;
+  const auto L = bindings.costQuadraticApproximation(t_arr[0], x_arr[0], u_arr[0]);
+  std::cout << "L: " << L.f << "\ndLdx: " << L.dfdx.transpose() << "\ndLdu: " << L.dfdu.transpose() << std::endl;
 
-  auto L = bindings.getIntermediateCost(t_arr[0], x_arr[0], u_arr[0]);
-  auto dLdu = bindings.getIntermediateCostDerivativeInput(t_arr[0], x_arr[0], u_arr[0]);
+  auto V = bindings.valueFunction(t_arr[0], x_arr[0]);
+  std::cout << "V: " << V << std::endl;
 
-  std::cout << "L: " << L << "\ndLdu: " << dLdu.transpose() << std::endl;
-
-  auto Vx = bindings.getValueFunctionStateDerivative(t_arr[0], x_arr[0]);
+  auto Vx = bindings.valueFunctionStateDerivative(t_arr[0], x_arr[0]);
   std::cout << "Vx: " << Vx.transpose() << std::endl;
 
-  auto e = bindings.getStateInputConstraint(t_arr[0], x_arr[0], u_arr[0]);
+  auto e = bindings.stateInputEqualityConstraint(t_arr[0], x_arr[0], u_arr[0]);
   std::cout << "e: " << e.transpose() << std::endl;
 
-  auto D = bindings.getStateInputConstraintDerivativeControl(t_arr[0], x_arr[0], u_arr[0]);
+  auto D = bindings.stateInputEqualityConstraintLinearApproximation(t_arr[0], x_arr[0], u_arr[0]).dfdu;
   std::cout << "D:\n" << D << std::endl;
 
-  auto nu = bindings.getStateInputConstraintLagrangian(t_arr[0], x_arr[0]);
+  auto nu = bindings.stateInputEqualityConstraintLagrangian(t_arr[0], x_arr[0], u_arr[0]);
   std::cout << "nu " << nu.transpose() << std::endl;
 
   bindings.reset(costDesiredTraj);
 
   t = 0.0;
   for (int i = 0; i < 301; i++) {
-    bindings.setObservation(t, initState);
+    bindings.setObservation(t, initState, initInput);
     bindings.advanceMpc();
     bindings.getMpcSolution(t_arr, x_arr, u_arr);
     t += 0.01;
   }
 
   bindings.reset(costDesiredTraj);
-  bindings.setObservation(0.0, initState);
+  bindings.setObservation(t, initState, initInput);
   bindings.advanceMpc();
 }
 

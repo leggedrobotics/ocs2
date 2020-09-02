8a22a7e829cd6f5832ce05db09b61e43d465e444
Michael Spieler <mspieler@ethz.ch>
2020-09-02 19:08:35 +0200

    Avoid pinocchio headers in PinocchioGeometryInterface header

--- ocs2_robotic_examples/ocs2_mobile_manipulator_example/include/ocs2_mobile_manipulator_example/PinocchioGeometryInterface.hpp
+++ ocs2_robotic_examples/ocs2_mobile_manipulator_example/include/ocs2_mobile_manipulator_example/PinocchioGeometryInterface.hpp
@@ -10,31 +10,31 @@
 #include <ocs2_mobile_manipulator_example/PinocchioInterface.h>
 
 #include <hpp/fcl/collision_data.h>
-#include <pinocchio/fwd.hpp>
-#include <pinocchio/multibody/data.hpp>
-#include <pinocchio/multibody/geometry.hpp>
-#include <pinocchio/multibody/model.hpp>
 
-namespace mobile_manipulator {
+/* Forward declaration of pinocchio geometry types */
+namespace pinocchio {
+struct GeometryModel;
+}  // namespace pinocchio
+
+namespace ocs2 {
 
 class PinocchioGeometryInterface {
  public:
-  PinocchioGeometryInterface(const std::string& urdfPath, PinocchioInterface<double>& pinocchioInterface,
-                             const pinocchio::GeometryModel::CollisionPairVector collisionPairs);
+  PinocchioGeometryInterface(const std::string& urdfPath, PinocchioInterface<scalar_t>& pinocchioInterface,
+                             const std::vector<std::pair<size_t, size_t>>& collisionPairs);
   virtual ~PinocchioGeometryInterface() = default;
 
   // TODO(perry) discussion over appropriate depth of copy/clone (ie should the interface ptrs or interfaces be copied)
   PinocchioGeometryInterface(const PinocchioGeometryInterface& other);
 
-  pinocchio::GeometryModel& getGeometryModel() { return geometryModel_; }
-  const pinocchio::GeometryModel& getGeometryModel() const { return geometryModel_; }
+  pinocchio::GeometryModel& getGeometryModel() { return *geometryModelPtr_; }
+  const pinocchio::GeometryModel& getGeometryModel() const { return *geometryModelPtr_; }
 
-  std::vector<hpp::fcl::DistanceResult> computeDistances(const Eigen::Matrix<double, Eigen::Dynamic, 1>& q);
+  std::vector<hpp::fcl::DistanceResult> computeDistances(const Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>& q);
 
  private:
-  PinocchioInterface<double> pinocchioInterface_;
-
-  pinocchio::GeometryModel geometryModel_;
+  PinocchioInterface<scalar_t> pinocchioInterface_;
+  std::shared_ptr<pinocchio::GeometryModel> geometryModelPtr_;
 };
 
-}  // namespace mobile_manipulator
+}  // namespace ocs2

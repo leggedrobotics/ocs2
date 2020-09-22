9c87e2dee52e2d1540cc7ee93b6375f41753073a
rgrandia <rubengrandia@gmail.com>
2020-09-22 20:26:34 +0200

    adapt bear to new model

--- anymal_bear/ocs2_anymal_bear/src/AnymalBearInterface.cpp
+++ anymal_bear/ocs2_anymal_bear/src/AnymalBearInterface.cpp
@@ -6,20 +6,19 @@
 
 #include <ros/package.h>
 
-#include <ocs2_anymal_bear_switched_model/core/AnymalBearCom.h>
-#include <ocs2_anymal_bear_switched_model/core/AnymalBearKinematics.h>
+#include <ocs2_anymal_models/AnymalModels.h>
 
 namespace anymal {
 
 std::unique_ptr<switched_model::QuadrupedPointfootInterface> getAnymalBearInterface(const std::string& taskFolder) {
   std::cerr << "Loading task file from: " << taskFolder << std::endl;
 
-  auto kin = AnymalBearKinematics();
-  auto kinAd = AnymalBearKinematicsAd();
-  auto com = AnymalBearCom();
-  auto comAd = AnymalBearComAd();
+  auto kin = getAnymalKinematics(AnymalModel::Bear);
+  auto kinAd = getAnymalKinematicsAd(AnymalModel::Bear);
+  auto com = getAnymalComModel(AnymalModel::Bear);
+  auto comAd = getAnymalComModelAd(AnymalModel::Bear);
   return std::unique_ptr<switched_model::QuadrupedPointfootInterface>(
-      new switched_model::QuadrupedPointfootInterface(kin, kinAd, com, comAd, taskFolder));
+      new switched_model::QuadrupedPointfootInterface(*kin, *kinAd, *com, *comAd, taskFolder));
 }
 
 std::string getTaskFileFolderBear(const std::string& taskName) {
--- anymal_bear/ocs2_anymal_bear_loopshaping/src/AnymalBearLoopshapingInterface.cpp
+++ anymal_bear/ocs2_anymal_bear_loopshaping/src/AnymalBearLoopshapingInterface.cpp
@@ -8,22 +8,13 @@
 
 #include <ocs2_quadruped_interface/QuadrupedPointfootInterface.h>
 
-#include <ocs2_anymal_bear_switched_model/core/AnymalBearCom.h>
-#include <ocs2_anymal_bear_switched_model/core/AnymalBearKinematics.h>
+#include <ocs2_anymal_bear/AnymalBearInterface.h>
 
 namespace anymal {
 
 std::unique_ptr<switched_model_loopshaping::QuadrupedLoopshapingInterface> getAnymalBearLoopshapingInterface(
     const std::string& taskFolder) {
-  std::cerr << "Loading task file from: " << taskFolder << std::endl;
-
-  auto kin = AnymalBearKinematics();
-  auto kinAd = AnymalBearKinematicsAd();
-  auto com = AnymalBearCom();
-  auto comAd = AnymalBearComAd();
-
-  auto quadrupedInterface = std::unique_ptr<switched_model::QuadrupedPointfootInterface>(
-      new switched_model::QuadrupedPointfootInterface(kin, kinAd, com, comAd, taskFolder));
+  auto quadrupedInterface = getAnymalBearInterface(taskFolder);
 
   return std::unique_ptr<switched_model_loopshaping::QuadrupedLoopshapingInterface>(
       new switched_model_loopshaping::QuadrupedLoopshapingInterface(std::move(quadrupedInterface), taskFolder));

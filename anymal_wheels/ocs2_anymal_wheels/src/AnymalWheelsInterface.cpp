028a12a2435ab04533fe2a34e2c660519581034a
rgrandia <rubengrandia@gmail.com>
2020-09-22 20:26:26 +0200

    adapt wheels to new model

--- anymal_wheels/ocs2_anymal_wheels/src/AnymalWheelsInterface.cpp
+++ anymal_wheels/ocs2_anymal_wheels/src/AnymalWheelsInterface.cpp
@@ -6,20 +6,19 @@
 
 #include <ros/package.h>
 
-#include <ocs2_anymal_wheels_switched_model/core/AnymalWheelsCom.h>
-#include <ocs2_anymal_wheels_switched_model/core/AnymalWheelsKinematics.h>
+#include <ocs2_anymal_models/AnymalModels.h>
 
 namespace anymal {
 
 std::unique_ptr<switched_model::QuadrupedWheeledInterface> getAnymalWheelsInterface(const std::string& taskFolder) {
   std::cerr << "Loading task file from: " << taskFolder << std::endl;
 
-  auto kin = AnymalWheelsKinematics();
-  auto kinAd = AnymalWheelsKinematicsAd();
-  auto com = AnymalWheelsCom();
-  auto comAd = AnymalWheelsComAd();
+  auto kin = getAnymalKinematics(AnymalModel::Wheels);
+  auto kinAd = getAnymalKinematicsAd(AnymalModel::Wheels);
+  auto com = getAnymalComModel(AnymalModel::Wheels);
+  auto comAd = getAnymalComModelAd(AnymalModel::Wheels);
   return std::unique_ptr<switched_model::QuadrupedWheeledInterface>(
-      new switched_model::QuadrupedWheeledInterface(kin, kinAd, com, comAd, taskFolder));
+      new switched_model::QuadrupedWheeledInterface(*kin, *kinAd, *com, *comAd, taskFolder));
 }
 
 std::string getTaskFileFolderWheels(const std::string& taskName) {
--- anymal_wheels/ocs2_anymal_wheels_loopshaping/src/AnymalWheelsLoopshapingInterface.cpp
+++ anymal_wheels/ocs2_anymal_wheels_loopshaping/src/AnymalWheelsLoopshapingInterface.cpp
@@ -6,24 +6,13 @@
 
 #include <ros/package.h>
 
-#include <ocs2_quadruped_interface/QuadrupedWheeledInterface.h>
-
-#include <ocs2_anymal_wheels_switched_model/core/AnymalWheelsCom.h>
-#include <ocs2_anymal_wheels_switched_model/core/AnymalWheelsKinematics.h>
+#include <ocs2_anymal_wheels/AnymalWheelsInterface.h>
 
 namespace anymal {
 
 std::unique_ptr<switched_model_loopshaping::QuadrupedLoopshapingInterface> getAnymalWheelsLoopshapingInterface(
     const std::string& taskFolder) {
-  std::cerr << "Loading task file from: " << taskFolder << std::endl;
-
-  auto kin = AnymalWheelsKinematics();
-  auto kinAd = AnymalWheelsKinematicsAd();
-  auto com = AnymalWheelsCom();
-  auto comAd = AnymalWheelsComAd();
-
-  auto quadrupedInterface = std::unique_ptr<switched_model::QuadrupedWheeledInterface>(
-      new switched_model::QuadrupedWheeledInterface(kin, kinAd, com, comAd, taskFolder));
+  auto quadrupedInterface = getAnymalWheelsInterface(taskFolder);
 
   return std::unique_ptr<switched_model_loopshaping::QuadrupedLoopshapingInterface>(
       new switched_model_loopshaping::QuadrupedLoopshapingInterface(std::move(quadrupedInterface), taskFolder));

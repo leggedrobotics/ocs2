eb9169b3782cdd6a46785be25da41a864d5dd9a2
rgrandia <rubengrandia@gmail.com>
2020-09-25 16:50:56 +0200

    add back anymal model tests

---
+++ ocs2_anymal_models/test/TestAnymalWheels.cpp
@@ -0,0 +1,40 @@
+//
+// Created by rgrandia on 25.09.20.
+//
+
+#include "TestAnymalSwitchedModel.h"
+
+#include <ocs2_anymal_models/AnymalModels.h>
+
+using namespace anymal;
+
+class AnymalWheelsSwitchedModelTests : public switched_model::TestAnymalSwitchedModel {
+ public:
+  AnymalWheelsSwitchedModelTests()
+      : TestAnymalSwitchedModel(getAnymalKinematics(AnymalModel::Wheels), getAnymalKinematicsAd(AnymalModel::Wheels),
+                                getAnymalComModel(AnymalModel::Wheels), getAnymalComModelAd(AnymalModel::Wheels)) {}
+};
+
+TEST_F(AnymalWheelsSwitchedModelTests, Constraints) {
+  this->testConstraints();
+}
+
+TEST_F(AnymalWheelsSwitchedModelTests, Kinematics) {
+  this->printKinematics();
+}
+
+TEST_F(AnymalWheelsSwitchedModelTests, ComDynamics) {
+  this->printComModel();
+}
+
+TEST_F(AnymalWheelsSwitchedModelTests, EndeffectorOrientation) {
+  this->testEndeffectorOrientation();
+}
+
+TEST_F(AnymalWheelsSwitchedModelTests, EndeffectorAlignedYAxisRandomHFEKFE) {
+  this->testEndeffectorAlignedYAxisRandomHFEKFE();
+}
+
+TEST_F(AnymalWheelsSwitchedModelTests, EndeffectorAlignedXAxisRandomHAA) {
+  this->testEndeffectorAlignedXAxisRandomHAA();
+}

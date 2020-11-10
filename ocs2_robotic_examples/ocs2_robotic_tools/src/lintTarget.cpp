86f618ded33fb20ecb727a30857caf3de008f665
Farbod Farshidian <farbod.farshidian@gmail.com>
2020-11-10 22:24:31 +0100

    clang tidy

--- ocs2_robotic_examples/ocs2_robotic_tools/src/lintTarget.cpp
+++ ocs2_robotic_examples/ocs2_robotic_tools/src/lintTarget.cpp
@@ -2,8 +2,8 @@
 #include <ocs2_robotic_tools/command/TargetTrajectoriesJoystickInterface.h>
 #include <ocs2_robotic_tools/command/TargetTrajectoriesKeyboardInterface.h>
 #include <ocs2_robotic_tools/common/AngularVelocityMapping.h>
-#include <ocs2_robotic_tools/common/RobotInterface.h>
 #include <ocs2_robotic_tools/common/LoopshapingRobotInterface.h>
+#include <ocs2_robotic_tools/common/RobotInterface.h>
 #include <ocs2_robotic_tools/common/RotationTransforms.h>
 
 // dummy target for clang toolchain

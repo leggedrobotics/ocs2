81d129594186ff8cb7a535ee5f69ede996979b56
rgrandia <rubengrandia@gmail.com>
2021-02-16 16:26:25 +0100

    add state input AD cost with Gauss Newton Hessian approximation

---
+++ ocs2_core/include/ocs2_core/cost/StateInputGaussNewtonCostAd.h
@@ -0,0 +1,96 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#pragma once
+
+#include "ocs2_core/cost/StateInputCost.h"
+
+#include <ocs2_core/automatic_differentiation/CppAdInterface.h>
+
+namespace ocs2 {
+
+/**
+ * State-input cost term of the form  0.5 ||f(x,u,p)||^2, where the linear approximation of f(x,u,p) will be used to form the Hessian
+ */
+class StateInputCostGaussNewtonAd : public StateInputCost {
+ public:
+  StateInputCostGaussNewtonAd(size_t stateDim, size_t inputDim);
+  ~StateInputCostGaussNewtonAd() override = default;
+  StateInputCostGaussNewtonAd* clone() const override = 0;
+
+  /**
+   * Initializes model libraries
+   *
+   * @param modelName : name of the generate model library
+   * @param modelFolder : folder to save the model library files to
+   * @param recompileLibraries : If true, the model library will be newly compiled. If false, an existing library will be loaded if
+   * available.
+   * @param verbose : print information.
+   */
+  void initialize(const std::string& modelName, const std::string& modelFolder = "/tmp/ocs2", bool recompileLibraries = true,
+                  bool verbose = true);
+
+  scalar_t getValue(scalar_t time, const vector_t& state, const vector_t& input,
+                    const CostDesiredTrajectories& desiredTrajectory) const override;
+
+  ScalarFunctionQuadraticApproximation getQuadraticApproximation(scalar_t time, const vector_t& state, const vector_t& input,
+                                                                 const CostDesiredTrajectories& desiredTrajectory) const override;
+
+ protected:
+  StateInputCostGaussNewtonAd(const StateInputCostGaussNewtonAd& rhs);
+
+  /** Gets a user-defined cost parameters. */
+  virtual vector_t getParameters(scalar_t time, const CostDesiredTrajectories& desiredTrajectory) const { return vector_t(0); }
+
+  /** Number of parameters for the cost function. */
+  virtual size_t getNumParameters() const { return 0; }
+
+  /**
+   * Interface method to the cost function. This method must be implemented by the derived class.
+   *
+   * @tparam scalar type. All the floating point operations should be with this type.
+   * @param [in] time: time.
+   * @param [in] state: state vector.
+   * @param [in] input: input vector.
+   * @param [in] parameters: parameter vector.
+   * @return vector of cost values f(x,u,p)
+   */
+  virtual ad_vector_t costVectorFunction(ad_scalar_t time, const ad_vector_t& state, const ad_vector_t& input,
+                                         const ad_vector_t& parameters) const = 0;
+
+ private:
+  /** Sets all the required CppAdCodeGenInterfaces */
+  void setADInterfaces(const std::string& modelName, const std::string& modelFolder);
+
+  size_t stateDim_;
+  size_t inputDim_;
+  std::unique_ptr<CppAdInterface> costVectorFunctionADInterfacePtr_;
+};
+
+}  // namespace ocs2
---
+++ ocs2_core/src/cost/StateInputGaussNewtonCostAd.cpp
@@ -0,0 +1,112 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#include <ocs2_core/cost/StateInputGaussNewtonCostAd.h>
+
+namespace ocs2 {
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+StateInputCostGaussNewtonAd::StateInputCostGaussNewtonAd(size_t stateDim, size_t inputDim)
+    : StateInputCost(), stateDim_(stateDim), inputDim_(inputDim) {}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+StateInputCostGaussNewtonAd::StateInputCostGaussNewtonAd(const StateInputCostGaussNewtonAd& rhs)
+    : StateInputCost(rhs),
+      stateDim_(rhs.stateDim_),
+      inputDim_(rhs.inputDim_),
+      costVectorFunctionADInterfacePtr_(new CppAdInterface(*rhs.costVectorFunctionADInterfacePtr_)) {}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+void StateInputCostGaussNewtonAd::initialize(const std::string& modelName, const std::string& modelFolder, bool recompileLibraries,
+                                             bool verbose) {
+  setADInterfaces(modelName, modelFolder);
+  if (recompileLibraries) {
+    costVectorFunctionADInterfacePtr_->createModels(CppAdInterface::ApproximationOrder::First, verbose);
+  } else {
+    costVectorFunctionADInterfacePtr_->loadModelsIfAvailable(CppAdInterface::ApproximationOrder::First, verbose);
+  }
+}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+scalar_t StateInputCostGaussNewtonAd::getValue(scalar_t time, const vector_t& state, const vector_t& input,
+                                               const CostDesiredTrajectories& desiredTrajectory) const {
+  vector_t timeStateInput(1 + stateDim_ + inputDim_);
+  timeStateInput << time, state, input;
+  const auto parameters = getParameters(time, desiredTrajectory);
+  const auto costVector = costVectorFunctionADInterfacePtr_->getFunctionValue(timeStateInput, parameters);
+  return 0.5 * costVector.squaredNorm();
+}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+ScalarFunctionQuadraticApproximation StateInputCostGaussNewtonAd::getQuadraticApproximation(
+    scalar_t time, const vector_t& state, const vector_t& input, const CostDesiredTrajectories& desiredTrajectory) const {
+  vector_t timeStateInput(1 + stateDim_ + inputDim_);
+  timeStateInput << time, state, input;
+  const auto parameters = getParameters(time, desiredTrajectory);
+  const auto costVector = costVectorFunctionADInterfacePtr_->getFunctionValue(timeStateInput, parameters);
+  auto costVectorJacobian = costVectorFunctionADInterfacePtr_->getJacobian(timeStateInput, parameters);
+
+  matrix_t JtJ = matrix_t::Zero(1 + stateDim_ + inputDim_, 1 + stateDim_ + inputDim_);
+  JtJ.selfadjointView<Eigen::Lower>().rankUpdate(costVectorJacobian.adjoint());
+
+  ScalarFunctionQuadraticApproximation L;
+  L.f = 0.5 * costVector.squaredNorm();
+  L.dfdx.noalias() = costVectorJacobian.middleCols(1, stateDim_).transpose() * costVector;
+  L.dfdxx.noalias() = costVectorJacobian.middleCols(1, stateDim_).transpose() * costVectorJacobian.middleCols(1, stateDim_);
+  L.dfdu.noalias() = costVectorJacobian.rightCols(inputDim_).transpose() * costVector;
+  L.dfduu.noalias() = costVectorJacobian.rightCols(inputDim_).transpose() * costVectorJacobian.rightCols(inputDim_);
+  L.dfdux.noalias() = costVectorJacobian.rightCols(inputDim_).transpose() * costVectorJacobian.middleCols(1, stateDim_);
+  return L;
+}
+
+/******************************************************************************************************/
+/******************************************************************************************************/
+/******************************************************************************************************/
+void StateInputCostGaussNewtonAd::setADInterfaces(const std::string& modelName, const std::string& modelFolder) {
+  auto costVector = [this](const ad_vector_t& x, const ad_vector_t& p, ad_vector_t& y) {
+    auto time = x(0);
+    auto state = x.segment(1, stateDim_);
+    auto input = x.tail(inputDim_);
+    y = this->costVectorFunction(time, state, input, p);
+  };
+  costVectorFunctionADInterfacePtr_.reset(
+      new CppAdInterface(costVector, 1 + stateDim_ + inputDim_, getNumParameters(), modelName + "_GN_cost_vector", modelFolder));
+}
+
+}  // namespace ocs2

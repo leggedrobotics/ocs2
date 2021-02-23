87fbb4858218693a35080328f4d3d477558f407b
Michael Spieler <mspieler@ethz.ch>
2021-02-23 13:48:50 +0100

    Fix RobotStatePublisher for Noetic

--- common/ocs2_quadruped_interface/src/QuadrupedVisualizer.cpp
+++ common/ocs2_quadruped_interface/src/QuadrupedVisualizer.cpp
@@ -110,8 +110,8 @@ void QuadrupedVisualizer::publishJointTransforms(ros::Time timeStamp, const join
                                                  {"RF_HAA", jointAngles[3]}, {"RF_HFE", jointAngles[4]},  {"RF_KFE", jointAngles[5]},
                                                  {"LH_HAA", jointAngles[6]}, {"LH_HFE", jointAngles[7]},  {"LH_KFE", jointAngles[8]},
                                                  {"RH_HAA", jointAngles[9]}, {"RH_HFE", jointAngles[10]}, {"RH_KFE", jointAngles[11]}};
-    robotStatePublisherPtr_->publishTransforms(jointPositions, timeStamp, "");
-    robotStatePublisherPtr_->publishFixedTransforms("");
+    robotStatePublisherPtr_->publishTransforms(jointPositions, timeStamp);
+    robotStatePublisherPtr_->publishFixedTransforms();
   }
 }
 

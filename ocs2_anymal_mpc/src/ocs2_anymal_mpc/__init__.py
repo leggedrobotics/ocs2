5bb55af37ea366a2b46b3b40907450effc33d1dd
Jan Carius <jan.carius@gmx.de>
2021-03-02 16:19:38 +0100

    fix anymal python bindings

--- ocs2_anymal_mpc/src/ocs2_anymal_mpc/__init__.py
+++ ocs2_anymal_mpc/src/ocs2_anymal_mpc/__init__.py
@@ -1,2 +1,2 @@
-from AnymalPyBindings import mpc_interface
-from AnymalPyBindings import scalar_array, vector_array, matrix_array, CostDesiredTrajectories
+from ocs2_anymal_mpc.AnymalPyBindings import mpc_interface
+from ocs2_anymal_mpc.AnymalPyBindings import scalar_array, vector_array, matrix_array, CostDesiredTrajectories

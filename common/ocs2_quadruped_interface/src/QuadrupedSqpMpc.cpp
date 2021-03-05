232e7b884664ffbe776a8d20344dbb284c6dbce5
rgrandia <rubengrandia@gmail.com>
2021-03-05 17:50:13 +0100

    add operating points

--- common/ocs2_quadruped_interface/src/QuadrupedSqpMpc.cpp
+++ common/ocs2_quadruped_interface/src/QuadrupedSqpMpc.cpp
@@ -9,9 +9,9 @@ namespace switched_model {
 std::unique_ptr<ocs2::MultipleShootingMpc> getSqpMpc(const QuadrupedInterface& quadrupedInterface, const ocs2::mpc::Settings& mpcSettings,
                                                      const ocs2::MultipleShootingSolverSettings& sqpSettings) {
   if (!quadrupedInterface.modelSettings().gaitOptimization_) {
-    auto mpcPtr = std::unique_ptr<ocs2::MultipleShootingMpc>(
-        new ocs2::MultipleShootingMpc(mpcSettings, sqpSettings, &quadrupedInterface.getDynamics(), &quadrupedInterface.getCost(),
-                                      quadrupedInterface.getConstraintPtr(), quadrupedInterface.getTerminalCostPtr()));
+    auto mpcPtr = std::unique_ptr<ocs2::MultipleShootingMpc>(new ocs2::MultipleShootingMpc(
+        mpcSettings, sqpSettings, &quadrupedInterface.getDynamics(), &quadrupedInterface.getCost(), quadrupedInterface.getConstraintPtr(),
+        quadrupedInterface.getTerminalCostPtr(), &quadrupedInterface.getOperatingPoints()));
     mpcPtr->getSolverPtr()->setModeScheduleManager(quadrupedInterface.getModeScheduleManagerPtr());
     return mpcPtr;
   } else {

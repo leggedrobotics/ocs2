e93c93373a87f8ecfa67811739913620d251d32e
Farbod Farshidian <farbod.farshidian@gmail.com>
2021-06-04 12:45:09 +0000

    BouncingMassTest.cpp edited online with Bitbucket

--- ocs2_ddp/test/bouncingmass/BouncingMassTest.cpp
+++ ocs2_ddp/test/bouncingmass/BouncingMassTest.cpp
@@ -206,6 +206,6 @@ TEST(BouncingMassTest, state_rollout_slq) {
 
   // Test 2: Check of cost function
   auto performanceIndeces = slq.getPerformanceIndeces();
-  constexpr scalar_t expectedCost = 7.2;
+  constexpr scalar_t expectedCost = 7.15;
   EXPECT_LT(std::fabs(performanceIndeces.totalCost - expectedCost), 100 * ddpSettings.minRelCost_);
 }

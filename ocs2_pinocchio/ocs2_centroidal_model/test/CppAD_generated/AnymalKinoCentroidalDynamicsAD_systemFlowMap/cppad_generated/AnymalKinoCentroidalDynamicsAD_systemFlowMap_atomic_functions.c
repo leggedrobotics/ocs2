1313dcfab68d41633d85d62c2790a8a8ca98cd53
Jean-Pierre <jpsleiman7@gmail.com>
2021-06-11 18:58:10 +0200

    delete cppad files

--- ocs2_pinocchio/ocs2_centroidal_model/test/CppAD_generated/AnymalKinoCentroidalDynamicsAD_systemFlowMap/cppad_generated/AnymalKinoCentroidalDynamicsAD_systemFlowMap_atomic_functions.c
+++
@@ -1,7 +0,0 @@
-void AnymalKinoCentroidalDynamicsAD_systemFlowMap_atomic_functions(const char*** names,
-                                                                   unsigned long* n) {
-   static const char* atomic[0] = {};
-   *names = atomic;
-   *n = 0;
-}
-

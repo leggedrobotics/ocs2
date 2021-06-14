1ab3c1f59501b45f8ca003ad541148842006d82d
rgrandia <rubengrandia@gmail.com>
2021-06-14 17:12:21 +0200

    new initializer interface

--- common/ocs2_quadruped_loopshaping_interface/src/QuadrupedLoopshapingSqpMpc.cpp
+++ common/ocs2_quadruped_loopshaping_interface/src/QuadrupedLoopshapingSqpMpc.cpp
@@ -10,7 +10,7 @@ std::unique_ptr<ocs2::MultipleShootingMpc> getSqpMpc(const QuadrupedLoopshapingI
                                                      const ocs2::mpc::Settings& mpcSettings,
                                                      const ocs2::multiple_shooting::Settings& sqpSettings) {
   auto mpcPtr = std::unique_ptr<ocs2::MultipleShootingMpc>(new ocs2::MultipleShootingMpc(
-      mpcSettings, sqpSettings, &quadrupedInterface.getDynamics(), &quadrupedInterface.getCost(), &quadrupedInterface.getOperatingPoints(),
+      mpcSettings, sqpSettings, &quadrupedInterface.getDynamics(), &quadrupedInterface.getCost(), &quadrupedInterface.getInitializer(),
       quadrupedInterface.getConstraintPtr(), quadrupedInterface.getTerminalCostPtr()));
   mpcPtr->getSolverPtr()->setModeScheduleManager(quadrupedInterface.getModeScheduleManagerPtr());
   return mpcPtr;

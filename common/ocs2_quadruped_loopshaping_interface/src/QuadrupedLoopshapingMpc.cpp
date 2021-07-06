608b3fef7433482b0a1d738cf7d43e26e1d455cc
rgrandia <rubengrandia@gmail.com>
2021-07-06 20:31:32 +0200

    change factories again

--- common/ocs2_quadruped_loopshaping_interface/src/QuadrupedLoopshapingMpc.cpp
+++ common/ocs2_quadruped_loopshaping_interface/src/QuadrupedLoopshapingMpc.cpp
@@ -5,21 +5,26 @@
 #include "ocs2_quadruped_loopshaping_interface/QuadrupedLoopshapingMpc.h"
 
 #include <ocs2_mpc/MPC_DDP.h>
+#include <ocs2_sqp/MultipleShootingMpc.h>
 
 namespace switched_model_loopshaping {
 
-std::unique_ptr<ocs2::MPC_BASE> getSlqMpc(const QuadrupedLoopshapingInterface& quadrupedInterface, const ocs2::mpc::Settings& mpcSettings,
-                                          const ocs2::ddp::Settings& ddpSettings,
-                                          std::shared_ptr<ocs2::ReferenceManagerInterface> alternativeReferenceManager) {
+std::unique_ptr<ocs2::MPC_BASE> getDdpMpc(const QuadrupedLoopshapingInterface& quadrupedInterface, const ocs2::mpc::Settings& mpcSettings,
+                                          const ocs2::ddp::Settings& ddpSettings) {
   std::unique_ptr<ocs2::MPC_BASE> mpcPtr(new ocs2::MPC_DDP(&quadrupedInterface.getRollout(), &quadrupedInterface.getDynamics(),
                                                            quadrupedInterface.getConstraintPtr(), &quadrupedInterface.getCost(),
                                                            &quadrupedInterface.getInitializer(), ddpSettings, mpcSettings,
                                                            quadrupedInterface.getTerminalCostPtr()));
-  if (alternativeReferenceManager) {
-    mpcPtr->getSolverPtr()->setReferenceManager(alternativeReferenceManager);
-  } else {
-    mpcPtr->getSolverPtr()->setReferenceManager(quadrupedInterface.getReferenceManagerPtr());
-  }
+  mpcPtr->getSolverPtr()->setReferenceManager(quadrupedInterface.getReferenceManagerPtr());
+  return mpcPtr;
+}
+
+std::unique_ptr<ocs2::MPC_BASE> getSqpMpc(const QuadrupedLoopshapingInterface& quadrupedInterface, const ocs2::mpc::Settings& mpcSettings,
+                                          const ocs2::multiple_shooting::Settings& sqpSettings) {
+  auto mpcPtr = std::unique_ptr<ocs2::MultipleShootingMpc>(new ocs2::MultipleShootingMpc(
+      mpcSettings, sqpSettings, &quadrupedInterface.getDynamics(), &quadrupedInterface.getCost(), &quadrupedInterface.getInitializer(),
+      quadrupedInterface.getConstraintPtr(), quadrupedInterface.getTerminalCostPtr()));
+  mpcPtr->getSolverPtr()->setReferenceManager(quadrupedInterface.getReferenceManagerPtr());
   return mpcPtr;
 }
 

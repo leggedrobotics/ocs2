68480d29263a92fd1cd4b9ec9ff8dc0f5508261d
Mayank Mittal <mittalma@leggedrobotics.com>
2021-07-17 11:30:58 +0200

    splits the ocs2-quadrotor into ros and non-ros

--- ocs2_robotic_examples_ros/ocs2_quadrotor_ros/src/QuadrotorDummyVisualization.cpp
+++ ocs2_robotic_examples_ros/ocs2_quadrotor_ros/src/QuadrotorDummyVisualization.cpp
@@ -27,7 +27,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
-#include "ocs2_quadrotor/ros_comm/QuadrotorDummyVisualization.h"
+#include "ocs2_quadrotor_ros/QuadrotorDummyVisualization.h"
 
 namespace ocs2 {
 namespace quadrotor {

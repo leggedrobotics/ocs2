dc96ac867380a89d914ec35e7d6588e1bb71b2d9
Farbod Farshidian <farbod.farshidian@gmail.com>
2021-07-20 18:51:19 +0200

    fixing pybindings

--- ocs2_robotic_examples/ocs2_double_integrator/include/ocs2_double_integrator/DoubleIntegratorPyBindings.h
+++ ocs2_robotic_examples/ocs2_double_integrator/include/ocs2_double_integrator/DoubleIntegratorPyBindings.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include <ocs2_double_integrator/DoubleIntegratorInterface.h>
-#include <ocs2_double_integrator/definitions.h>
+#include <ocs2_mpc/MPC_DDP.h>
 #include <ocs2_python_interface/PythonInterface.h>
+
+#include "ocs2_double_integrator/DoubleIntegratorInterface.h"
+#include "ocs2_double_integrator/definitions.h"
 
 namespace ocs2 {
 namespace double_integrator {
@@ -10,8 +12,17 @@ namespace double_integrator {
 class DoubleIntegratorPyBindings final : public PythonInterface {
  public:
   explicit DoubleIntegratorPyBindings(const std::string& taskFileFolder) {
+    // Robot interface
     DoubleIntegratorInterface doubleIntegratorInterface(taskFileFolder);
-    PythonInterface::init(doubleIntegratorInterface, doubleIntegratorInterface.getMpc());
+
+    // MPC
+    std::unique_ptr<MPC_DDP> mpcPtr(new MPC_DDP(
+        doubleIntegratorInterface.mpcSettings(), doubleIntegratorInterface.ddpSettings(), doubleIntegratorInterface.getRollout(),
+        doubleIntegratorInterface.getOptimalControlProblem(), doubleIntegratorInterface.getInitializer()));
+    mpcPtr->getSolverPtr()->setReferenceManager(doubleIntegratorInterface.getReferenceManagerPtr());
+
+    // Python interface
+    PythonInterface::init(doubleIntegratorInterface, std::move(mpcPtr));
   }
 };
 

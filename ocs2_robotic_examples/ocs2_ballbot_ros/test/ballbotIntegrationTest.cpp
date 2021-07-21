d573496bc9dce1163140789fa45412c14d10721d
Mayank Mittal <mittalma@leggedrobotics.com>
2021-07-21 16:26:30 +0200

    adds ros packages to meta package for robotic examples

--- ocs2_robotic_examples/ocs2_ballbot_ros/test/ballbotIntegrationTest.cpp
+++ ocs2_robotic_examples/ocs2_ballbot_ros/test/ballbotIntegrationTest.cpp
@@ -42,7 +42,7 @@ using namespace ocs2;
 
 TEST(BallbotIntegrationTest, createDummyMRT) {
   const std::string taskFile = ros::package::getPath("ocs2_ballbot") + "/config/mpc/task.info";
-  const std::string libFolder = ros::package::getPath("ocs2_ballbot") + "auto_generated";
+  const std::string libFolder = ros::package::getPath("ocs2_ballbot") + "/auto_generated";
   ballbot::BallbotInterface ballbotInterface(taskFile, libFolder);
 
   MRT_ROS_Interface mrt("ballbot");
@@ -60,7 +60,7 @@ TEST(BallbotIntegrationTest, createDummyMRT) {
 
 TEST(BallbotIntegrationTest, createMPC) {
   const std::string taskFile = ros::package::getPath("ocs2_ballbot") + "/config/mpc/task.info";
-  const std::string libFolder = ros::package::getPath("ocs2_ballbot") + "auto_generated";
+  const std::string libFolder = ros::package::getPath("ocs2_ballbot") + "/auto_generated";
   ballbot::BallbotInterface ballbotInterface(taskFile, libFolder);
 
   // MPC

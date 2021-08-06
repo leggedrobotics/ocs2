5f02ca2bc5b6524dd4fd6c7e354669a731477f81
Farbod Farshidian <farbod.farshidian@gmail.com>
2021-08-06 23:39:21 +0200

    fixing relative path in raisim

--- ocs2_raisim/ocs2_raisim/src/RaisimRollout.cpp
+++ ocs2_raisim/ocs2_raisim/src/RaisimRollout.cpp
@@ -27,7 +27,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
-#include "../../../ocs2_raisim/ocs2_raisim/include/ocs2_raisim/RaisimRollout.h"
+#include "ocs2_raisim/RaisimRollout.h"
 
 namespace ocs2 {
 

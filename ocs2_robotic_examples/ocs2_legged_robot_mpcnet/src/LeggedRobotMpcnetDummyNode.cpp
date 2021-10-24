ca8b7092e553a83b45c6c364b5572bbcdd5b919b
Alexander Reske <areske@leggedrobotics.com>
2021-10-24 17:37:57 +0200

    add raisim option for legged robot mpcnet dummy

--- ocs2_robotic_examples/ocs2_legged_robot_mpcnet/src/LeggedRobotMpcnetDummyNode.cpp
+++ ocs2_robotic_examples/ocs2_legged_robot_mpcnet/src/LeggedRobotMpcnetDummyNode.cpp
@@ -1,14 +1,19 @@
-#include <ros/ros.h>
+#include <ros/init.h>
+#include <ros/package.h>
 #include <urdf_parser/urdf_parser.h>
 
 #include <ocs2_centroidal_model/CentroidalModelPinocchioMapping.h>
 #include <ocs2_legged_robot/LeggedRobotInterface.h>
 #include <ocs2_legged_robot/gait/GaitReceiver.h>
 #include <ocs2_legged_robot/visualization/LeggedRobotVisualizer.h>
+#include <ocs2_legged_robot_raisim/LeggedRobotRaisimConversions.h>
+#include <ocs2_legged_robot_raisim/LeggedRobotRaisimVisualizer.h>
 #include <ocs2_mpcnet/control/MpcnetOnnxController.h>
 #include <ocs2_mpcnet/dummy/MpcnetDummyLoopRos.h>
 #include <ocs2_mpcnet/dummy/MpcnetDummyObserverRos.h>
 #include <ocs2_pinocchio_interface/PinocchioEndEffectorKinematics.h>
+#include <ocs2_raisim/RaisimRollout.h>
+#include <ocs2_raisim_ros/RaisimHeightmapRosConverter.h>
 #include <ocs2_ros_interfaces/synchronized_module/RosReferenceManager.h>
 
 #include "ocs2_legged_robot_mpcnet/LeggedRobotMpcnetDefinition.h"
@@ -19,15 +24,16 @@ using namespace legged_robot;
 int main(int argc, char** argv) {
   std::vector<std::string> programArgs{};
   ::ros::removeROSArgs(argc, argv, programArgs);
-  if (programArgs.size() < 6) {
+  if (programArgs.size() < 7) {
     throw std::runtime_error(
-        "No robot name, config folder, target command file, description name, or policy file path specified. Aborting.");
+        "No robot name, config folder, target command file, description name, policy file path, or rollout type specified. Aborting.");
   }
   const std::string robotName(programArgs[1]);
   const std::string configName(programArgs[2]);
   const std::string targetCommandFile(programArgs[3]);
   const std::string descriptionName("/" + programArgs[4]);
   const std::string policyFilePath(programArgs[5]);
+  const bool raisim = (programArgs[6] == "true") ? true : false;
 
   // initialize ros node
   ros::init(argc, argv, robotName + "_mpcnet_dummy");
@@ -56,7 +62,39 @@ int main(int argc, char** argv) {
   mpcnetControllerPtr->loadPolicyModel(policyFilePath);
 
   // rollout
-  std::unique_ptr<RolloutBase> rolloutPtr(leggedRobotInterface.getRollout().clone());
+  std::unique_ptr<RolloutBase> rolloutPtr;
+  raisim::HeightMap* terrain = nullptr;
+  std::unique_ptr<RaisimHeightmapRosConverter> heightmapPub;
+  std::unique_ptr<LeggedRobotRaisimConversions> conversions;
+  if (raisim) {
+    conversions.reset(new LeggedRobotRaisimConversions(leggedRobotInterface.getPinocchioInterface(),
+                                                       leggedRobotInterface.getCentroidalModelInfo(), leggedRobotInterface.modelSettings(),
+                                                       false));
+    RaisimRolloutSettings raisimRolloutSettings(ros::package::getPath("ocs2_legged_robot_raisim") + "/config/raisim.info", "rollout", true);
+    conversions->setGains(raisimRolloutSettings.pGains_, raisimRolloutSettings.dGains_);
+    rolloutPtr.reset(
+        new RaisimRollout(ros::package::getPath("anymal_c_simple_description") + "/urdf/anymal.urdf",
+                          ros::package::getPath("anymal_c_simple_description") + "/meshes",
+                          std::bind(&LeggedRobotRaisimConversions::stateToRaisimGenCoordGenVel, conversions.get(), std::placeholders::_1,
+                                    std::placeholders::_2),
+                          std::bind(&LeggedRobotRaisimConversions::raisimGenCoordGenVelToState, conversions.get(), std::placeholders::_1,
+                                    std::placeholders::_2),
+                          std::bind(&LeggedRobotRaisimConversions::inputToRaisimGeneralizedForce, conversions.get(), std::placeholders::_1,
+                                    std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
+                          nullptr, raisimRolloutSettings, nullptr));
+    // terrain
+    if (raisimRolloutSettings.generateTerrain_) {
+      raisim::TerrainProperties terrainProperties;
+      terrainProperties.zScale = raisimRolloutSettings.terrainRoughness_;
+      terrainProperties.seed = raisimRolloutSettings.terrainSeed_;
+      terrain = static_cast<RaisimRollout*>(rolloutPtr.get())->generateTerrain(terrainProperties);
+      conversions->terrain_ = terrain;
+      heightmapPub.reset(new ocs2::RaisimHeightmapRosConverter());
+      heightmapPub->publishGridmap(*terrain, "odom");
+    }
+  } else {
+    rolloutPtr.reset(leggedRobotInterface.getRollout().clone());
+  }
 
   // observer
   std::shared_ptr<MpcnetDummyObserverRos> mpcnetDummyObserverRosPtr(new MpcnetDummyObserverRos(nodeHandle, robotName));
@@ -65,8 +103,15 @@ int main(int argc, char** argv) {
   CentroidalModelPinocchioMapping pinocchioMapping(leggedRobotInterface.getCentroidalModelInfo());
   PinocchioEndEffectorKinematics endEffectorKinematics(leggedRobotInterface.getPinocchioInterface(), pinocchioMapping,
                                                        leggedRobotInterface.modelSettings().contactNames3DoF);
-  std::shared_ptr<LeggedRobotVisualizer> leggedRobotVisualizerPtr(new LeggedRobotVisualizer(
-      leggedRobotInterface.getPinocchioInterface(), leggedRobotInterface.getCentroidalModelInfo(), endEffectorKinematics, nodeHandle));
+  std::shared_ptr<LeggedRobotVisualizer> leggedRobotVisualizerPtr;
+  if (raisim) {
+    leggedRobotVisualizerPtr.reset(new LeggedRobotRaisimVisualizer(leggedRobotInterface.getPinocchioInterface(),
+                                                                   leggedRobotInterface.getCentroidalModelInfo(), endEffectorKinematics,
+                                                                   nodeHandle, 100.0, terrain));
+  } else {
+    leggedRobotVisualizerPtr.reset(new LeggedRobotVisualizer(
+        leggedRobotInterface.getPinocchioInterface(), leggedRobotInterface.getCentroidalModelInfo(), endEffectorKinematics, nodeHandle));
+  }
 
   // MPC-Net dummy loop ROS
   scalar_t controlFrequency = leggedRobotInterface.mpcSettings().mrtDesiredFrequency_;

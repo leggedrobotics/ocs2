459fea8e2c196399b2cb1c143e073e4c6969605a
Farbod Farshidian <farbod.farshidian@gmail.com>
2022-01-24 20:22:18 +0100

    completing testMultiplier

--- ocs2_core/test/augmented_lagrangian/testMultiplier.cpp
+++ ocs2_core/test/augmented_lagrangian/testMultiplier.cpp
@@ -27,6 +27,8 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
 
+#include <numeric>
+
 #include <gtest/gtest.h>
 
 #include <ocs2_core/augmented_lagrangian/Multiplier.h>
@@ -81,4 +83,97 @@ inline MultiplierCollection interpolateNew(const LinearInterpolation::index_alph
     return dataArray[0];
   }
 }
+
+void random(const ocs2::size_array_t& termsSize, vector_t& serializedMultiplier, std::vector<Multiplier>& multiplier) {
+  const size_t length = std::accumulate(termsSize.begin(), termsSize.end(), termsSize.size());
+  serializedMultiplier = ocs2::vector_t::Random(length);
+  multiplier = ocs2::toMultipliers(termsSize, serializedMultiplier);
+}
+
+bool isApprox(const ocs2::MultiplierCollection& lhs, const ocs2::MultiplierCollection& rhs, scalar_t prec) {
+  bool flag = toVector(lhs.stateEq).isApprox(toVector(rhs.stateEq), prec);
+  flag = flag && toVector(lhs.stateIneq).isApprox(toVector(rhs.stateIneq), prec);
+  flag = flag && toVector(lhs.stateInputEq).isApprox(toVector(rhs.stateInputEq), prec);
+  flag = flag && toVector(lhs.stateInputIneq).isApprox(toVector(rhs.stateInputIneq), prec);
+  return flag;
+}
+
 }  // namespace ocs2
+
+TEST(TestMultiplier, testSerialization) {
+  const ocs2::size_array_t termsSize {0, 2, 0, 0, 3, 5};
+  const size_t numConstraint = termsSize.size();
+  const size_t length = std::accumulate(termsSize.begin(), termsSize.end(), numConstraint);
+
+  const ocs2::vector_t randomVec = ocs2::vector_t::Random(length);
+  const std::vector<ocs2::Multiplier> termsMultiplier = ocs2::toMultipliers(termsSize, randomVec);
+  const ocs2::vector_t serializedMultiplier = ocs2::toVector(termsMultiplier);
+
+  EXPECT_TRUE(serializedMultiplier == randomVec);
+  EXPECT_TRUE(serializedMultiplier.size() == length);
+  EXPECT_TRUE(getSizes(termsMultiplier) == termsSize);
+}
+
+TEST(TestMultiplier, testSwap) {
+  const ocs2::size_array_t termsSize {0, 2, 0, 0, 3, 5};
+
+  ocs2::vector_t serializedStateEq, serializedStateIneq, serializedStateInputEq, serializedStateInputIneq;
+
+  ocs2::MultiplierCollection termsMultiplier;
+  ocs2::random(termsSize, serializedStateEq, termsMultiplier.stateEq);
+  ocs2::random(termsSize, serializedStateIneq, termsMultiplier.stateIneq);
+  ocs2::random(termsSize, serializedStateInputEq, termsMultiplier.stateInputEq);
+  ocs2::random(termsSize, serializedStateInputIneq, termsMultiplier.stateInputIneq);
+
+  auto termsMultiplierRef = termsMultiplier;
+  ocs2::MultiplierCollection termsMultiplierNew;
+  ocs2::swap(termsMultiplier, termsMultiplierNew);
+
+  EXPECT_TRUE(isApprox(termsMultiplierNew, termsMultiplierRef, 1e-10));
+
+  clear(termsMultiplierRef);
+  EXPECT_TRUE(isApprox(termsMultiplier, termsMultiplierRef, 1e-10));
+}
+
+TEST(TestMultiplier, testInterpolation) {
+  // terms size
+  const ocs2::size_array_t stateEqTermsSize {0, 0};
+  const ocs2::size_array_t stateIneqTermsSize {2};
+  const ocs2::size_array_t stateInputEqTermsSize {};
+  const ocs2::size_array_t stateInputIneqTermsSize {0, 2, 0, 0, 3, 5};
+
+  // construct trajectories of length N
+  const size_t N = 11;
+  ocs2::scalar_array_t timeTrajectory(N);
+  ocs2::vector_array_t stateEqTrajectory(N);
+  ocs2::vector_array_t stateIneqTrajectory(N);
+  ocs2::vector_array_t stateInputEqTrajectory(N);
+  ocs2::vector_array_t stateInputIneqTrajectory(N);
+  std::vector<ocs2::MultiplierCollection> multiplierCollectionTrajectory(N);
+  for (size_t i = 0; i < N; i++) {
+    timeTrajectory[i] = i * 0.1;
+    ocs2::random(stateEqTermsSize, stateEqTrajectory[i], multiplierCollectionTrajectory[i].stateEq);
+    ocs2::random(stateIneqTermsSize, stateIneqTrajectory[i], multiplierCollectionTrajectory[i].stateIneq);
+    ocs2::random(stateInputEqTermsSize, stateInputEqTrajectory[i], multiplierCollectionTrajectory[i].stateInputEq);
+    ocs2::random(stateInputIneqTermsSize, stateInputIneqTrajectory[i], multiplierCollectionTrajectory[i].stateInputIneq);
+  }  // end of i loop
+
+  constexpr ocs2::scalar_t prec = 1e-8;
+  const ocs2::scalar_array_t timeTrajectoryTest {-1.0, 0.0, 4.0, 4.6, 8.7, 10.0, 11.0, 100.0};
+  for (size_t i = 0; i < timeTrajectoryTest.size(); i++) {
+    const auto indexAlpha = ocs2::LinearInterpolation::timeSegment(timeTrajectoryTest[i], timeTrajectory);
+
+    const auto stateEq = ocs2::LinearInterpolation::interpolate(indexAlpha, stateEqTrajectory);
+    const auto stateIneq = ocs2::LinearInterpolation::interpolate(indexAlpha, stateIneqTrajectory);
+    const auto stateInputEq = ocs2::LinearInterpolation::interpolate(indexAlpha, stateInputEqTrajectory);
+    const auto stateInputIneq = ocs2::LinearInterpolation::interpolate(indexAlpha, stateInputIneqTrajectory);
+    const auto multiplierCollection = ocs2::LinearInterpolation::interpolate(indexAlpha, multiplierCollectionTrajectory);
+    const auto multiplierCollectionNew = interpolateNew(indexAlpha, multiplierCollectionTrajectory);
+
+    EXPECT_TRUE(stateEq.isApprox(toVector(multiplierCollection.stateEq), prec));
+    EXPECT_TRUE(stateIneq.isApprox(toVector(multiplierCollection.stateIneq), prec));
+    EXPECT_TRUE(stateInputEq.isApprox(toVector(multiplierCollection.stateInputEq), prec));
+    EXPECT_TRUE(stateInputIneq.isApprox(toVector(multiplierCollection.stateInputIneq), prec));
+    EXPECT_TRUE(isApprox(multiplierCollection, multiplierCollectionNew, prec));
+  }  // end of i loop
+}

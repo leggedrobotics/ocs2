ae8a9e1ce9f1437aae8577b3e400868833a801c4
Farbod Farshidian <farbod.farshidian@gmail.com>
2022-01-27 17:23:24 +0100

    adding sqp solver to legged robot

---
+++ ocs2_robotic_examples/ocs2_legged_robot_ros/src/MultipleShootingLeggedRobotMpcNode.cpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+Copyright (c) 2017, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+ * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+#include <ros/init.h>
+
+#include <ocs2_legged_robot/LeggedRobotInterface.h>
+#include <ocs2_ros_interfaces/mpc/MPC_ROS_Interface.h>
+#include <ocs2_ros_interfaces/synchronized_module/RosReferenceManager.h>
+#include <ocs2_sqp/MultipleShootingMpc.h>
+
+#include "ocs2_legged_robot_ros/gait/GaitReceiver.h"
+
+using namespace ocs2;
+using namespace legged_robot;
+
+int main(int argc, char** argv) {
+  std::vector<std::string> programArgs{};
+  ::ros::removeROSArgs(argc, argv, programArgs);
+  if (programArgs.size() < 5) {
+    throw std::runtime_error("No robot name, config folder, target command file, or description name specified. Aborting.");
+  }
+  const std::string robotName(programArgs[1]);
+  const std::string taskFile(programArgs[2]);
+  const std::string urdfFile(programArgs[3]);
+  const std::string referenceFile(programArgs[4]);
+
+  // Initialize ros node
+  ros::init(argc, argv, robotName + "_mpc");
+  ros::NodeHandle nodeHandle;
+
+  // Robot interface
+  LeggedRobotInterface interface(taskFile, urdfFile, referenceFile);
+
+  // Gait receiver
+  auto gaitReceiverPtr =
+      std::make_shared<GaitReceiver>(nodeHandle, interface.getSwitchedModelReferenceManagerPtr()->getGaitSchedule(), robotName);
+
+  // ROS ReferenceManager
+  auto rosReferenceManagerPtr = std::make_shared<RosReferenceManager>(robotName, interface.getReferenceManagerPtr());
+  rosReferenceManagerPtr->subscribe(nodeHandle);
+
+  // MPC
+  MultipleShootingMpc mpc(interface.mpcSettings(), interface.sqpSettings(), interface.getOptimalControlProblem(),
+                          interface.getInitializer());
+  mpc.getSolverPtr()->setReferenceManager(rosReferenceManagerPtr);
+  mpc.getSolverPtr()->addSynchronizedModule(gaitReceiverPtr);
+
+  // Launch MPC ROS node
+  MPC_ROS_Interface mpcNode(mpc, robotName);
+  mpcNode.launchNodes(nodeHandle);
+
+  // Successful exit
+  return 0;
+}

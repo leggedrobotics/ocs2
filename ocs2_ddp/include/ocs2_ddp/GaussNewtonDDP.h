9404cfb40db23483542aeff13cd2cf7e6d126576
Farbod Farshidian <farbod.farshidian@gmail.com>
2022-03-03 16:00:44 +0100

    remove GaussNewtonDDP::correctcachedTrajectoryTail

--- ocs2_ddp/include/ocs2_ddp/GaussNewtonDDP.h
+++ ocs2_ddp/include/ocs2_ddp/GaussNewtonDDP.h
@@ -365,20 +365,6 @@ class GaussNewtonDDP : public SolverBase {
   void correctInitcachedNominalTrajectories();
 
   /**
-   * Corrects for the tail of the cached trajectory based on the nominal trajectory. This compensates for the
-   * the moving horizon (MPC) applications where the final time of the cached trajectory is smaller than the
-   * nominal one.
-   *
-   * @param [in] timeSegment: The interval index and interpolation coefficient alpha of the cached trajectory final
-   * time in the nominal time trajectory.
-   * @param [in] currentTrajectory: The nominal trajectory.
-   * @param [out] cachedTrajectory: The cached trajectory.
-   */
-  template <typename Data_T, class Alloc>
-  static void correctcachedTrajectoryTail(std::pair<int, scalar_t> timeSegment, const std::vector<Data_T, Alloc>& currentTrajectory,
-                                          std::vector<Data_T, Alloc>& cachedTrajectory);
-
-  /**
    * Runs the initialization method for Gauss-Newton DDP.
    */
   void runInit();
@@ -464,21 +450,5 @@ class GaussNewtonDDP : public SolverBase {
   benchmark::RepeatedTimer computeControllerTimer_;
   benchmark::RepeatedTimer searchStrategyTimer_;
 };
-
-/******************************************************************************************************/
-/******************************************************************************************************/
-/******************************************************************************************************/
-template <typename Data_T, class Alloc>
-void GaussNewtonDDP::correctcachedTrajectoryTail(std::pair<int, scalar_t> timeSegment, const std::vector<Data_T, Alloc>& currentTrajectory,
-                                                 std::vector<Data_T, Alloc>& cachedTrajectory) {
-  // adding the fist cashed value
-  Data_T firstCachedValue = LinearInterpolation::interpolate(timeSegment, currentTrajectory);
-  cachedTrajectory.emplace_back(std::move(firstCachedValue));
-
-  // Concatenate the rest
-  const int ignoredSizeOfNominal = timeSegment.first + 1;
-
-  cachedTrajectory.insert(cachedTrajectory.end(), currentTrajectory.begin() + ignoredSizeOfNominal, currentTrajectory.end());
-}
 
 }  // namespace ocs2

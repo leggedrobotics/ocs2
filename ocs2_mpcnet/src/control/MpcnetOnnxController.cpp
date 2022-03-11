601619779d302b783fb1aaca34e7ef4f2b499de8
Alexander Reske <areske@leggedrobotics.com>
2022-03-11 16:34:29 +0100

    cleanup ort inference

--- ocs2_mpcnet/src/control/MpcnetOnnxController.cpp
+++ ocs2_mpcnet/src/control/MpcnetOnnxController.cpp
@@ -31,23 +31,21 @@ void MpcnetOnnxController::loadPolicyModel(const std::string& policyFilePath) {
 /******************************************************************************************************/
 vector_t MpcnetOnnxController::computeInput(const scalar_t t, const vector_t& x) {
   // create input tensor objects
-  Eigen::Matrix<tensor_element_t, Eigen::Dynamic, 1> tEigenData = getGeneralizedTime(t).cast<tensor_element_t>();
-  Eigen::Matrix<tensor_element_t, Eigen::Dynamic, 1> xEigenData = getRelativeState(t, x).cast<tensor_element_t>();
-  std::vector<tensor_element_t> tData(tEigenData.data(), tEigenData.data() + tEigenData.size());
-  std::vector<tensor_element_t> xData(xEigenData.data(), xEigenData.data() + xEigenData.size());
+  Eigen::Matrix<tensor_element_t, Eigen::Dynamic, 1> time = getGeneralizedTime(t).cast<tensor_element_t>();
+  Eigen::Matrix<tensor_element_t, Eigen::Dynamic, 1> state = getRelativeState(t, x).cast<tensor_element_t>();
   Ort::MemoryInfo memoryInfo = Ort::MemoryInfo::CreateCpu(OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
   std::vector<Ort::Value> inputValues;
   inputValues.push_back(
-      Ort::Value::CreateTensor<tensor_element_t>(memoryInfo, tData.data(), tData.size(), inputShapes_[0].data(), inputShapes_[0].size()));
+      Ort::Value::CreateTensor<tensor_element_t>(memoryInfo, time.data(), time.size(), inputShapes_[0].data(), inputShapes_[0].size()));
   inputValues.push_back(
-      Ort::Value::CreateTensor<tensor_element_t>(memoryInfo, xData.data(), xData.size(), inputShapes_[1].data(), inputShapes_[1].size()));
+      Ort::Value::CreateTensor<tensor_element_t>(memoryInfo, state.data(), state.size(), inputShapes_[1].data(), inputShapes_[1].size()));
   // run inference
   Ort::RunOptions runOptions;
-  std::vector<Ort::Value> outputValues = sessionPtr_->Run(runOptions, inputNames_.data(), inputValues.data(), 2, outputNames_.data(), 2);
-  // evaluate output tensor objects (note that from u, p, U we only need u = U * p which is already evaluated by the model)
-  Eigen::Map<Eigen::Matrix<tensor_element_t, Eigen::Dynamic, 1>> u(outputValues[0].GetTensorMutableData<tensor_element_t>(),
-                                                                   outputShapes_[0][1], outputShapes_[0][0]);
-  return getInputTransformation(t, x) * u.cast<scalar_t>();
+  std::vector<Ort::Value> outputValues = sessionPtr_->Run(runOptions, inputNames_.data(), inputValues.data(), 2, outputNames_.data(), 1);
+  // evaluate output tensor objects
+  Eigen::Map<Eigen::Matrix<tensor_element_t, Eigen::Dynamic, 1>> input(outputValues[0].GetTensorMutableData<tensor_element_t>(),
+                                                                       outputShapes_[0][1], outputShapes_[0][0]);
+  return getInputTransformation(t, x) * input.cast<scalar_t>();
 }
 
 }  // namespace ocs2

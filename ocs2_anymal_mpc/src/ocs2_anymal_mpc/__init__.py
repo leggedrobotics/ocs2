e2d77cce082e6a08c01f40f791a5aba6f91c23a1
rgrandia <rubengrandia@gmail.com>
2022-04-27 15:17:00 +0200

    remove anymal pybindings

--- ocs2_anymal_mpc/src/ocs2_anymal_mpc/__init__.py
+++
@@ -1,2 +0,0 @@
-from ocs2_anymal_mpc.AnymalPyBindings import mpc_interface
-from ocs2_anymal_mpc.AnymalPyBindings import scalar_array, vector_array, matrix_array, TargetTrajectories
--- ocs2_anymal_mpc/src/pyBindModule.cpp
+++
@@ -1,5 +0,0 @@
-#include <ocs2_anymal_mpc/AnymalPyBindings.h>
-
-#include <ocs2_python_interface/PybindMacros.h>
-
-CREATE_ROBOT_PYTHON_BINDINGS(anymal::AnymalPyBindings, AnymalPyBindings);

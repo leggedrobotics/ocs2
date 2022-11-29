33ea8f24e637004a00a1223f53d97dc17c8acab8
Farbod Farshidian <farbod.farshidian@gmail.com>
2022-11-29 20:14:18 +0100

    extracting general member methods as functions

---
+++ ocs2_sqp/ocs2_sqp/include/ocs2_sqp/MultipleShootingHelpers.h
@@ -0,0 +1,118 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#pragma once
+
+#include <ocs2_core/Types.h>
+#include <ocs2_oc/oc_data/PerformanceIndex.h>
+#include <ocs2_oc/oc_data/PrimalSolution.h>
+
+#include "ocs2_sqp/MultipleShootingSolverStatus.h"
+#include "ocs2_sqp/TimeDiscretization.h"
+
+namespace ocs2 {
+namespace multiple_shooting {
+
+/** Compute 2-norm of the trajectory: sqrt(sum_i v[i]^2) */
+inline scalar_t trajectoryNorm(const vector_array_t& v) {
+  scalar_t norm = 0.0;
+  for (const auto& vi : v) {
+    norm += vi.squaredNorm();
+  }
+  return std::sqrt(norm);
+}
+
+/** Increment the given trajectory as: vNew[i] = v[i] + alpha * dv[i]. It assumes that vNew is already resized to size of v. */
+template <typename Type>
+void incrementTrajectory(const std::vector<Type>& v, const std::vector<Type>& dv, const scalar_t alpha, std::vector<Type>& vNew) {
+  assert(v.size() == dv.size());
+  if (v.size() != vNew.size()) {
+    throw std::runtime_error("[incrementTrajectory] Resize vNew to the size of v!");
+  }
+
+  for (int i = 0; i < v.size(); i++) {
+    if (dv[i].size() > 0) {  // account for absence of inputs at events.
+      vNew[i] = v[i] + alpha * dv[i];
+    } else {
+      vNew[i] = Type();
+    }
+  }
+}
+
+/**
+ * Computes the Armijo descent metric that determines if the solution is a descent direction for the cost. It calculates sum of
+ * gradient(cost).dot([dx; du]) over the trajectory.
+ *
+ * @param [in] cost: The quadratic approximation of the cost.
+ * @param [in] deltaXSol: The state trajectory of the QP subproblem solution.
+ * @param [in] deltaUSol: The input trajectory of the QP subproblem solution.
+ * @return The Armijo descent metric.
+ */
+scalar_t armijoDescentMetric(const std::vector<ScalarFunctionQuadraticApproximation>& cost, const vector_array_t& deltaXSol,
+                             const vector_array_t& deltaUSol);
+
+/**
+ * Re-map the projected input back to the original space.
+ *
+ * @param [in] constraintsProjection: The constraints projection.
+ * @param [in] deltaXSol: The state trajectory of the QP subproblem solution.
+ * @param [in, out] deltaUSol: The input trajectory of the QP subproblem solution.
+ */
+void remapProjectedInput(const std::vector<VectorFunctionLinearApproximation>& constraintsProjection, const vector_array_t& deltaXSol,
+                         vector_array_t& deltaUSol);
+
+void remapProjectedGain(const std::vector<VectorFunctionLinearApproximation>& constraintsProjection, matrix_array_t& KMatrices);
+
+/**
+ * Sets the primal solution (with a feedforward controller) based the LQ subproblem solution.
+ *
+ * @param [in] time : The annotated time trajectory
+ * @param [in] modeSchedule: The mode schedule.
+ * @param [in] x: The state trajectory of the QP subproblem solution.
+ * @param [in] u: The input trajectory of the QP subproblem solution.
+ * @param [out] the updated primal solution
+ */
+void setPrimalSolution(const std::vector<AnnotatedTime>& time, ModeSchedule&& modeSchedule, vector_array_t&& x, vector_array_t&& u,
+                       PrimalSolution& primalSolution);
+
+/**
+ * Sets the primal solution (with a linear controller) based the LQ subproblem solution.
+ *
+ * @param [in] time : The annotated time trajectory
+ * @param [in] modeSchedule: The mode schedule.
+ * @param [in] x: The state trajectory of the QP subproblem solution.
+ * @param [in] u: The input trajectory of the QP subproblem solution.
+ * @param [in] KMatrices: The LQR gain trajectory of the QP subproblem solution.
+ * @param [out] the updated primal solution
+ */
+void setPrimalSolution(const std::vector<AnnotatedTime>& time, ModeSchedule&& modeSchedule, vector_array_t&& x, vector_array_t&& u,
+                       matrix_array_t&& KMatrices, PrimalSolution& primalSolution);
+
+}  // namespace multiple_shooting
+}  // namespace ocs2
---
+++ ocs2_sqp/ocs2_sqp/src/MultipleShootingHelpers.cpp
@@ -0,0 +1,134 @@
+/******************************************************************************
+Copyright (c) 2020, Farbod Farshidian. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#include "ocs2_sqp/MultipleShootingHelpers.h"
+
+#include <ocs2_core/control/FeedforwardController.h>
+#include <ocs2_core/control/LinearController.h>
+
+namespace ocs2 {
+namespace multiple_shooting {
+
+scalar_t armijoDescentMetric(const std::vector<ScalarFunctionQuadraticApproximation>& cost, const vector_array_t& deltaXSol,
+                             const vector_array_t& deltaUSol) {
+  // To determine if the solution is a descent direction for the cost: compute gradient(cost)' * [dx; du]
+  scalar_t metric = 0.0;
+  for (int i = 0; i < cost.size(); i++) {
+    if (cost[i].dfdx.size() > 0) {
+      metric += cost[i].dfdx.dot(deltaXSol[i]);
+    }
+    if (cost[i].dfdu.size() > 0) {
+      metric += cost[i].dfdu.dot(deltaUSol[i]);
+    }
+  }
+  return metric;
+}
+
+void remapProjectedInput(const std::vector<VectorFunctionLinearApproximation>& constraintsProjection, const vector_array_t& deltaXSol,
+                         vector_array_t& deltaUSol) {
+  vector_t tmp;  // 1 temporary for re-use.
+  for (int i = 0; i < deltaUSol.size(); ++i) {
+    if (constraintsProjection[i].f.size() > 0) {
+      tmp.noalias() = constraintsProjection[i].dfdu * deltaUSol[i];
+      deltaUSol[i] = tmp + constraintsProjection[i].f;
+      deltaUSol[i].noalias() += constraintsProjection[i].dfdx * deltaXSol[i];
+    }
+  }
+}
+
+void remapProjectedGain(const std::vector<VectorFunctionLinearApproximation>& constraintsProjection, matrix_array_t& KMatrices) {
+  matrix_t tmp;  // 1 temporary for re-use.
+  for (int i = 0; i < KMatrices.size(); ++i) {
+    if (constraintsProjection[i].f.size() > 0) {
+      tmp.noalias() = constraintsProjection[i].dfdu * KMatrices[i];
+      KMatrices[i] = tmp + constraintsProjection[i].dfdx;
+    }
+  }
+}
+
+void setPrimalSolution(const std::vector<AnnotatedTime>& time, ModeSchedule&& modeSchedule, vector_array_t&& x, vector_array_t&& u,
+                       PrimalSolution& primalSolution) {
+  // Correct for missing inputs at PreEvents and the terminal time
+  for (int i = 0; i < u.size(); ++i) {
+    if (time[i].event == AnnotatedTime::Event::PreEvent && i > 0) {
+      u[i] = u[i - 1];
+    }
+  }
+  // Repeat last input to make equal length vectors
+  u.push_back(u.back());
+
+  // Construct nominal time, state and input trajectories
+  primalSolution.clear();
+  primalSolution.stateTrajectory_ = std::move(x);
+  primalSolution.inputTrajectory_ = std::move(u);
+  std::tie(primalSolution.timeTrajectory_, primalSolution.postEventIndices_) = toTime(time);
+  primalSolution.modeSchedule_ = std::move(modeSchedule);
+  primalSolution.controllerPtr_.reset(new FeedforwardController(primalSolution.timeTrajectory_, primalSolution.inputTrajectory_));
+}
+
+void setPrimalSolution(const std::vector<AnnotatedTime>& time, ModeSchedule&& modeSchedule, vector_array_t&& x, vector_array_t&& u,
+                       matrix_array_t&& KMatrices, PrimalSolution& primalSolution) {
+  // Compute feedback, before x and u are moved to primal solution
+  // see doc/LQR_full.pdf for detailed derivation for feedback terms
+  vector_array_t uff = u;  // Copy and adapt in loop
+  for (int i = 0; i < KMatrices.size(); ++i) {
+    if (time[i].event == AnnotatedTime::Event::PreEvent && i > 0) {
+      uff[i] = uff[i - 1];
+      KMatrices[i] = KMatrices[i - 1];
+    } else {
+      // Linear controller has convention u = uff + K * x;
+      // We computed u = u'(t) + K (x - x'(t));
+      // >> uff = u'(t) - K x'(t)
+      uff[i].noalias() -= KMatrices[i] * x[i];
+    }
+  }
+  // Copy last one to get correct length
+  uff.push_back(uff.back());
+  KMatrices.push_back(KMatrices.back());
+
+  // Correct for missing inputs at PreEvents and the terminal time
+  for (int i = 0; i < u.size(); ++i) {
+    if (time[i].event == AnnotatedTime::Event::PreEvent && i > 0) {
+      u[i] = u[i - 1];
+    }
+  }
+  // Repeat last input to make equal length vectors
+  u.push_back(u.back());
+
+  // Construct nominal time, state and input trajectories
+  primalSolution.clear();
+  primalSolution.stateTrajectory_ = std::move(x);
+  primalSolution.inputTrajectory_ = std::move(u);
+  std::tie(primalSolution.timeTrajectory_, primalSolution.postEventIndices_) = toTime(time);
+  primalSolution.modeSchedule_ = std::move(modeSchedule);
+  primalSolution.controllerPtr_.reset(new LinearController(primalSolution.timeTrajectory_, std::move(uff), std::move(KMatrices)));
+}
+
+}  // namespace multiple_shooting
+}  // namespace ocs2
